skzap/metamask-extension

Improve styling of advanced-tab-content gasInput row

ui/app/components/gas-customization/gas-modal-page-container/advanced-tab-content/advanced-tab-content.component.js
@@ -39,7 +39,7 @@ export default class AdvancedTabContent extends Component {
   }
 
   infoButton (onClick) {
-    return <i className="fa info-circle" onClick={onClick} />
+    return <i className="fa fa-info-circle" onClick={onClick} />
   }
 
   renderDataSummary (totalFee, millisecondsRemaining) {

aimaniac1989/metamask-review

ui/app/components/gas-customization/gas-modal-page-container/advanced-tab-content/advanced-tab-content.component.js
@@ -39,7 +39,7 @@ export default class AdvancedTabContent extends Component {
   }
 
   infoButton (onClick) {
-    return <i className="fa info-circle" onClick={onClick} />
+    return <i className="fas fa-info-circle" onClick={onClick} />
   }
 
   renderDataSummary (totalFee, millisecondsRemaining) {