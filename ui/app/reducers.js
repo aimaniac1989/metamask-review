willmartell/metamask-extension

Connect the gas-button-group component to redux and a live api.

ui/app/reducers.js
@@ -9,6 +9,7 @@ const reduceApp = require('./reducers/app')
 const reduceLocale = require('./reducers/locale')
 const reduceSend = require('./ducks/send.duck').default
 import reduceConfirmTransaction from './ducks/confirm-transaction.duck'
+import reduceGasEstimates from './ducks/gas-estimates.duck'
 
 window.METAMASK_CACHED_LOG_STATE = null
 
@@ -47,6 +48,8 @@ function rootReducer (state, action) {
   state.send = reduceSend(state, action)
 
   state.confirmTransaction = reduceConfirmTransaction(state, action)
+
+  state.gasEstimates = reduceGasEstimates(state, action)
 
   window.METAMASK_CACHED_LOG_STATE = state
   return state

aimaniac1989/metamask-review

ui/app/reducers.js
@@ -10,7 +10,7 @@ const reduceApp = require('./reducers/app')
 const reduceLocale = require('./reducers/locale')
 const reduceSend = require('./ducks/send.duck').default
 import reduceConfirmTransaction from './ducks/confirm-transaction.duck'
-import reduceCustomGas from './ducks/custom-gas'
+import reduceGas from './ducks/gas.duck'
 
 window.METAMASK_CACHED_LOG_STATE = null
 
@@ -50,7 +50,7 @@ function rootReducer (state, action) {
 
   state.confirmTransaction = reduceConfirmTransaction(state, action)
 
-  state.customGas = reduceCustomGas(state, action)
+  state.gas = reduceGas(state, action)
 
   window.METAMASK_CACHED_LOG_STATE = state
   return state