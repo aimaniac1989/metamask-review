aimaniac1989/metamask-review

commit e54f5a4738b9944da0825fa480b3c2d4949ef014

Cherry-pick d4207ce (PR #24086) to v11.14.1 (#24109)
Cherry-pick d4207ce (PR #24086) to v11.14.1

There were no merge conflicts.

ui/pages/confirmations/components/simulation-details/useBalanceChanges.ts
@@ -1,21 +1,19 @@
 import { Hex } from '@metamask/utils';
 import { useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
 import {
   SimulationBalanceChange,
   SimulationData,
   SimulationTokenBalanceChange,
   SimulationTokenStandard,
 } from '@metamask/transaction-controller';
+import { ContractExchangeRates } from '@metamask/assets-controllers';
 import { useAsyncResultOrThrow } from '../../../../hooks/useAsyncResult';
 import { getTokenStandardAndDetails } from '../../../../store/actions';
 import { TokenStandard } from '../../../../../shared/constants/transaction';
 import { Numeric } from '../../../../../shared/modules/Numeric';
 import { getConversionRate } from '../../../../ducks/metamask/metamask';
-import {
-  getConfirmationExchangeRates,
-  getTokenExchangeRates,
-} from '../../../../selectors';
+import { getCurrentChainId, getCurrentCurrency } from '../../../../selectors';
+import { fetchTokenExchangeRates } from '../../../../helpers/utils/util';
 import {
   Amount,
   BalanceChange,
@@ -25,11 +23,11 @@ import {
 } from './types';
 
 const NATIVE_DECIMALS = 18;
+
 const ERC20_DEFAULT_DECIMALS = 18;
-const EMPTY_TOKEN_BALANCE_CHANGES: SimulationTokenBalanceChange[] = [];
 
 // Converts a SimulationTokenStandard to a TokenStandard
-const convertStandard = (standard: SimulationTokenStandard) => {
+function convertStandard(standard: SimulationTokenStandard) {
   switch (standard) {
     case SimulationTokenStandard.erc20:
       return TokenStandard.ERC20;
@@ -40,39 +38,56 @@ const convertStandard = (standard: SimulationTokenStandard) => {
     default:
       throw new Error(`Unknown token standard: ${standard}`);
   }
-};
+}
 
 // Calculates the asset amount based on the balance change and decimals
-const getAssetAmount = (
+function getAssetAmount(
   { isDecrease: isNegative, difference: quantity }: SimulationBalanceChange,
   decimals: number,
-): Amount => {
+): Amount {
   const numeric = Numeric.from(quantity, 16)
     .times(isNegative ? -1 : 1, 10)
     .toBase(10)
     .shiftedBy(decimals);
   return { isNegative, quantity, decimals, numeric };
-};
+}
 
 // Fetches token details for all the token addresses in the SimulationTokenBalanceChanges
-const fetchErc20Decimals = async (
+async function fetchErc20Decimals(
   addresses: Hex[],
-): Promise<Record<Hex, number>> => {
-  const uniqueAddresses = [...new Set(addresses)];
+): Promise<Record<Hex, number>> {
+  const uniqueAddresses = [
+    ...new Set(addresses.map((address) => address.toLowerCase())),
+  ];
   const tokenInfos = await Promise.all(
     uniqueAddresses.map((address) => getTokenStandardAndDetails(address)),
   );
-
-  return tokenInfos.reduce(
-    (result, { decimals }, index) => ({
-      ...result,
-      [uniqueAddresses[index]]: decimals
-        ? parseInt(decimals, 10)
-        : ERC20_DEFAULT_DECIMALS,
-    }),
-    {},
+  return Object.fromEntries(
+    tokenInfos.map(({ decimals }, index) => [
+      uniqueAddresses[index],
+      decimals ? parseInt(decimals, 10) : ERC20_DEFAULT_DECIMALS,
+    ]),
   );
-};
+}
+
+async function fetchTokenFiatRates(
+  fiatCurrency: string,
+  erc20TokenAddresses: Hex[],
+  chainId: Hex,
+): Promise<ContractExchangeRates> {
+  const tokenRates = await fetchTokenExchangeRates(
+    fiatCurrency,
+    erc20TokenAddresses,
+    chainId,
+  );
+
+  return Object.fromEntries(
+    Object.entries(tokenRates).map(([address, rate]) => [
+      address.toLowerCase(),
+      rate,
+    ]),
+  );
+}
 
 // Compiles the balance change for the native asset
 function getNativeBalanceChange(
@@ -94,7 +109,7 @@ function getNativeBalanceChange(
 function getTokenBalanceChanges(
   tokenBalanceChanges: SimulationTokenBalanceChange[],
   erc20Decimals: Record<Hex, number>,
-  tokenFiatRates: Record<Hex, number>,
+  erc20FiatRates: Partial<Record<Hex, number>>,
 ): BalanceChange[] {
   return tokenBalanceChanges.map((tokenBc) => {
     const asset: TokenAssetIdentifier = {
@@ -107,7 +122,7 @@ function getTokenBalanceChanges(
       asset.standard === TokenStandard.ERC20 ? erc20Decimals[asset.address] : 0;
     const amount = getAssetAmount(tokenBc, decimals);
 
-    const fiatRate = tokenFiatRates[tokenBc.address];
+    const fiatRate = erc20FiatRates[tokenBc.address];
     const fiatAmount = fiatRate
       ? amount.numeric.applyConversionRate(fiatRate).toNumber()
       : FIAT_UNAVAILABLE;
@@ -116,49 +131,33 @@ function getTokenBalanceChanges(
   });
 }
 
-// Get the exchange rates for converting tokens to the user's fiat currency.
-const getTokenToFiatConversionRates = createSelector(
-  getConversionRate,
-  getTokenExchangeRates,
-  getConfirmationExchangeRates,
-  (nativeToFiat, contractExchangeRates, confirmationExchangeRates) => {
-    const mergedRates = {
-      ...contractExchangeRates,
-      ...confirmationExchangeRates,
-    } as Record<string, number>;
-    return Object.entries(mergedRates).reduce((acc, [key, value]) => {
-      acc[key.toLowerCase()] = nativeToFiat * value;
-      return acc;
-    }, {} as Record<string, number>);
-  },
-);
-
 // Compiles a list of balance changes from simulation data
 export const useBalanceChanges = (
-  simulationData?: Partial<SimulationData>,
+  simulationData: SimulationData | undefined,
 ): { pending: boolean; value: BalanceChange[] } => {
+  const chainId = useSelector(getCurrentChainId);
+  const fiatCurrency = useSelector(getCurrentCurrency);
   const nativeFiatRate = useSelector(getConversionRate);
-  const tokenFiatRates = useSelector(getTokenToFiatConversionRates);
 
-  const {
-    nativeBalanceChange,
-    tokenBalanceChanges = EMPTY_TOKEN_BALANCE_CHANGES,
-  } = simulationData ?? {};
+  const { nativeBalanceChange, tokenBalanceChanges = [] } =
+    simulationData ?? {};
 
-  const erc20Addresses = tokenBalanceChanges
+  const erc20TokenAddresses = tokenBalanceChanges
     .filter((tbc) => tbc.standard === SimulationTokenStandard.erc20)
-    .map((tbc) => tbc.address.toLowerCase() as Hex);
+    .map((tbc) => tbc.address);
 
-  const erc20Decimals = useAsyncResultOrThrow(() => {
-    return fetchErc20Decimals(erc20Addresses);
-  }, [JSON.stringify(erc20Addresses)]);
+  const erc20Decimals = useAsyncResultOrThrow(
+    () => fetchErc20Decimals(erc20TokenAddresses),
+    [JSON.stringify(erc20TokenAddresses)],
+  );
 
-  if (erc20Decimals.pending) {
+  const erc20FiatRates = useAsyncResultOrThrow(
+    () => fetchTokenFiatRates(fiatCurrency, erc20TokenAddresses, chainId),
+    [JSON.stringify(erc20TokenAddresses), chainId, fiatCurrency],
+  );
+
+  if (erc20Decimals.pending || erc20FiatRates.pending || !simulationData) {
     return { pending: true, value: [] };
-  }
-
-  if (!simulationData) {
-    return { pending: false, value: [] };
   }
 
   const nativeChange = getNativeBalanceChange(
@@ -168,7 +167,7 @@ export const useBalanceChanges = (
   const tokenChanges = getTokenBalanceChanges(
     tokenBalanceChanges,
     erc20Decimals.value,
-    tokenFiatRates,
+    erc20FiatRates.value,
   );
 
   const balanceChanges: BalanceChange[] = [

ui/pages/confirmations/components/simulation-details/useBalanceChanges.test.ts
@@ -6,9 +6,9 @@ import {
 } from '@metamask/transaction-controller';
 import { TokenStandard } from '../../../../../shared/constants/transaction';
 import { getConversionRate } from '../../../../ducks/metamask/metamask';
-import { getTokenExchangeRates } from '../../../../selectors';
 import { getTokenStandardAndDetails } from '../../../../store/actions';
 import { Numeric } from '../../../../../shared/modules/Numeric';
+import { fetchTokenExchangeRates } from '../../../../helpers/utils/util';
 import { useBalanceChanges } from './useBalanceChanges';
 import { FIAT_UNAVAILABLE } from './types';
 
@@ -21,8 +21,12 @@ jest.mock('../../../../ducks/metamask/metamask', () => ({
 }));
 
 jest.mock('../../../../selectors', () => ({
-  getTokenExchangeRates: jest.fn(),
-  getConfirmationExchangeRates: jest.fn().mockReturnValue({}),
+  getCurrentChainId: jest.fn(),
+  getCurrentCurrency: jest.fn(),
+}));
+
+jest.mock('../../../../helpers/utils/util', () => ({
+  fetchTokenExchangeRates: jest.fn(),
 }));
 
 jest.mock('../../../../store/actions', () => ({
@@ -30,8 +34,8 @@ jest.mock('../../../../store/actions', () => ({
 }));
 
 const mockGetConversionRate = getConversionRate as jest.Mock;
-const mockGetTokenExchangeRates = getTokenExchangeRates as unknown as jest.Mock;
 const mockGetTokenStandardAndDetails = getTokenStandardAndDetails as jest.Mock;
+const mockFetchTokenExchangeRates = fetchTokenExchangeRates as jest.Mock;
 
 const ETH_TO_FIAT_RATE = 3;
 
@@ -39,8 +43,8 @@ const ERC20_TOKEN_ADDRESS_1_MOCK: Hex = '0x0erc20_1';
 const ERC20_TOKEN_ADDRESS_2_MOCK: Hex = '0x0erc20_2';
 const ERC20_DECIMALS_1_MOCK = 3;
 const ERC20_DECIMALS_2_MOCK = 4;
-const ERC20_TO_ETH_RATE_1_MOCK = 0.5;
-const ERC20_TO_ETH_RATE_2_MOCK = 2;
+const ERC20_TO_FIAT_RATE_1_MOCK = 1.5;
+const ERC20_TO_FIAT_RATE_2_MOCK = 6;
 
 const NFT_TOKEN_ADDRESS_MOCK: Hex = '0x0nft';
 
@@ -55,6 +59,11 @@ const dummyBalanceChange = {
   newBalance: '0xIGNORE' as Hex,
 };
 
+const PENDING_PROMISE = () =>
+  new Promise(() => {
+    /* unresolved promise */
+  });
+
 describe('useBalanceChanges', () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -68,38 +77,68 @@ describe('useBalanceChanges', () => {
       });
     });
     mockGetConversionRate.mockReturnValue(ETH_TO_FIAT_RATE);
-    mockGetTokenExchangeRates.mockReturnValue({
-      [ERC20_TOKEN_ADDRESS_1_MOCK]: ERC20_TO_ETH_RATE_1_MOCK,
-      [ERC20_TOKEN_ADDRESS_2_MOCK]: ERC20_TO_ETH_RATE_2_MOCK,
+    mockFetchTokenExchangeRates.mockResolvedValue({
+      [ERC20_TOKEN_ADDRESS_1_MOCK]: ERC20_TO_FIAT_RATE_1_MOCK,
+      [ERC20_TOKEN_ADDRESS_2_MOCK]: ERC20_TO_FIAT_RATE_2_MOCK,
     });
   });
 
-  it('handles no simulation data', async () => {
-    const { result, waitForNextUpdate } = renderHook(() =>
-      useBalanceChanges(undefined),
-    );
-    expect(result.current).toEqual({ pending: expect.any(Boolean), value: [] });
-    await waitForNextUpdate();
-  });
+  describe('pending states', () => {
+    it('returns pending=true if no simulation data', async () => {
+      const { result, waitForNextUpdate } = renderHook(() =>
+        useBalanceChanges(undefined),
+      );
+      expect(result.current).toEqual({ pending: true, value: [] });
+      await waitForNextUpdate();
+    });
 
-  it('returns pending state when fetching token details', async () => {
-    const simulationData: SimulationData = {
-      nativeBalanceChange: undefined,
-      tokenBalanceChanges: [
-        {
-          ...dummyBalanceChange,
-          difference: DIFFERENCE_1_MOCK,
-          isDecrease: true,
-          address: ERC20_TOKEN_ADDRESS_1_MOCK,
-          standard: SimulationTokenStandard.erc20,
-        },
-      ],
-    };
-    const { result, unmount } = renderHook(() =>
-      useBalanceChanges(simulationData),
-    );
-    expect(result.current).toEqual({ pending: true, value: [] });
-    unmount();
+    it('returns pending=true while fetching token decimals', async () => {
+      mockGetTokenStandardAndDetails.mockImplementation(PENDING_PROMISE);
+      const simulationData: SimulationData = {
+        nativeBalanceChange: undefined,
+        tokenBalanceChanges: [
+          {
+            ...dummyBalanceChange,
+            difference: DIFFERENCE_1_MOCK,
+            isDecrease: true,
+            address: ERC20_TOKEN_ADDRESS_1_MOCK,
+            standard: SimulationTokenStandard.erc20,
+          },
+        ],
+      };
+      const { result, unmount, waitForNextUpdate } = renderHook(() =>
+        useBalanceChanges(simulationData),
+      );
+
+      await waitForNextUpdate();
+
+      expect(result.current).toEqual({ pending: true, value: [] });
+      unmount();
+    });
+
+    it('returns pending=true while fetching token fiat rates', async () => {
+      mockFetchTokenExchangeRates.mockImplementation(PENDING_PROMISE);
+      const simulationData: SimulationData = {
+        nativeBalanceChange: undefined,
+        tokenBalanceChanges: [
+          {
+            ...dummyBalanceChange,
+            difference: DIFFERENCE_1_MOCK,
+            isDecrease: true,
+            address: ERC20_TOKEN_ADDRESS_1_MOCK,
+            standard: SimulationTokenStandard.erc20,
+          },
+        ],
+      };
+      const { result, unmount, waitForNextUpdate } = renderHook(() =>
+        useBalanceChanges(simulationData),
+      );
+
+      await waitForNextUpdate();
+
+      expect(result.current).toEqual({ pending: true, value: [] });
+      unmount();
+    });
   });
 
   describe('with token balance changes', () => {

commit 5538722bcde8c9f70937594e2cf99cdb68dcfe5e

lint fix type error introduced by #24109

ui/pages/confirmations/components/simulation-details/useBalanceChanges.ts
@@ -140,7 +140,7 @@ function getTokenBalanceChanges(
 
 // Compiles a list of balance changes from simulation data
 export const useBalanceChanges = (
-  simulationData: SimulationData | undefined,
+  simulationData?: Partial<SimulationData>,
 ): { pending: boolean; value: BalanceChange[] } => {
   const chainId = useSelector(getCurrentChainId);
   const fiatCurrency = useSelector(getCurrentCurrency);