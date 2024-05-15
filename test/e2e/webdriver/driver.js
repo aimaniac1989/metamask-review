MetaMask/metamask-extension

commit 527f12cd56d12c3a3bd68adfd1d7fdf3eb35629d

feat: Slightly update UI for the STX Opt In modal (#24525)

test/e2e/mmi/pageObjects/mmi-signup-page.ts
@@ -44,7 +44,9 @@ export class MMISignUpPage {
       'button:has-text("Confirm Secret Recovery Phrase")',
     );
     this.agreeBtn = page.locator('button:has-text("I agree")');
-    this.noThanksBtn = page.locator('button:has-text("No thanks")');
+    this.noThanksBtn = page.locator(
+      'button:has-text("Don\'t enable enhanced protection")',
+    );
     this.passwordTxt = page.getByTestId('create-password-new');
     this.passwordConfirmTxt = page.getByTestId('create-password-confirm');
     this.agreeCheck = page.getByTestId('create-new-vault__terms-checkbox');

test/e2e/vault-decryption-chrome.spec.js
@@ -50,7 +50,10 @@ async function getExtensionStorageFilePath(driver) {
  */
 async function closePopoverIfPresent(driver) {
   const popoverButtonSelector = '[data-testid="popover-close"]';
-  const linkNoThanks = { text: 'No thanks', tag: 'button' };
+  const linkNoThanks = {
+    text: "Don't enable enhanced protection",
+    tag: 'button',
+  };
   await driver.clickElementSafe(popoverButtonSelector);
   await driver.clickElementSafe(linkNoThanks);
 }

ui/components/app/smart-transactions/smart-transactions-opt-in-modal.tsx
@@ -89,7 +89,7 @@ const NoThanksLink = ({
       width={BlockSize.Full}
       className="mm-smart-transactions-opt-in-modal__no-thanks-link"
     >
-      {t('noThanks')}
+      {t('dontEnableEnhancedProtection')}
     </Button>
   );
 };
@@ -149,10 +149,7 @@ const Benefits = () => {
         text={t('smartTransactionsBenefit1')}
         iconName={IconName.Confirmation}
       />
-      <Benefit
-        text={t('smartTransactionsBenefit2')}
-        iconName={IconName.SecurityTick}
-      />
+      <Benefit text={t('smartTransactionsBenefit2')} iconName={IconName.Coin} />
       <Benefit
         text={t('smartTransactionsBenefit3')}
         iconName={IconName.Clock}

aimaniac1989/metamask-review

commit 1a020654d447752815842399422476fa1c8de248

fix: Use double quotes in in xpath text locator in driver.buildLocator (#24539)
## **Description**

`test/e2e/vault-decryption-chrome.spec.js` currently fails on `develop`.
This started failing after
https://github.com/MetaMask/metamask-extension/pull/24525 was merged.
The error was:

```
Element [object Object] not found (JavascriptError: javascript error: {"status":32,"value":"Unable to locate an element with the xpath expression //button[contains(text(), 'Don't enable enhanced protection')] because of the following error:\nSyntaxError: Failed to execute 'evaluate' on 'Document': The string '//button[contains(text(), 'Don't enable enhanced protection')]' is not a valid XPath expression."}
```

That PR changed a selector to select based on the text "Don't enable
enhanced protection". Having an unescaped single quote inside single
quotes was the problem. The solution in this PR is just to use double
quotes in the `By.xpath` call in the `buildLocator` driver method (that
all of our custom find functions depend on)

Note: the test that is failing only runs on the develop branch and on
release branches, which is why the PR was able to be merged.

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/24539?quickstart=1)

## **Manual testing steps**

1. Checkout this branch
2. Run `yarn && yarn dist`
3. Run `yarn test:e2e:single test/e2e/vault-decryption-chrome.spec.js
--browser chrome`

The tests should pass. Doing the same on `develop` results in the above
mentioned failure/error

## **Pre-merge author checklist**

- [ ] I’ve followed [MetaMask Coding
Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
- [ ] I've completed the PR template to the best of my ability
- [ ] I’ve included tests if applicable
- [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
if applicable

test/e2e/webdriver/driver.js
@@ -186,9 +186,16 @@ class Driver {
           .toXPath();
         return By.xpath(xpath);
       }
+      // If the text to be selected contains single or double quotation marks
+      // it can cause the xpath selector to be invalid. `textToLocate` results
+      // in a string that won't be invalidated by the presence of quotation
+      // marks within the text the test is trying to find
+      const textToLocate = locator.text.match(/"/u)
+        ? `'${locator.text}'`
+        : `"${locator.text}"`;
       // The tag prop is optional and further refines which elements match
       return By.xpath(
-        `//${locator.tag ?? '*'}[contains(text(), '${locator.text}')]`,
+        `//${locator.tag ?? '*'}[contains(text(), ${textToLocate})]`,
       );
     }
     throw new Error(