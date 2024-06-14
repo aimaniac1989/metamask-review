MetaMask/metamask-extension

commit 671c9975424a83904a4752dfb8a7cf728ae67355

feat: add `wallet_switchEthereumChain` permission and enforce it (behind feature flag) (#24415)
## **Description**

This PR introduces the `wallet_switchEthereumChain` permission (as an
`endowment` type permission) behind a feature flag
(`CHAIN_PERMISSIONS`), allowing us to remove the `wallet_switchEthereum`
confirmation. Now instead of seeing this confirmation everytime
`wallet_switchEthereumChain` is called for a given chain, users sees a
confirmation to add a permission for the dapp to switch to this chain
after which they will no longer see any confirmation at all when the
dapp next attempts to switch to this chain.

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/24415?quickstart=1)


## **Related issues**

Fixes: https://github.com/MetaMask/MetaMask-planning/issues/2327

## **Manual testing steps**

1. `CHAIN_PERMISSIONS=1 yarn start`
2. Go to a dapp (uniswap is good for this test) and connect the wallet
3. Use the in dapp switch chain UI
4. You should see a permission confirmation (instead of the old switch
chain confirmation)
- The UI is not fully implemented, this will be completed by the
`wallet-ux` team.
5. Manually switch to another chain in the wallet
6. Go back to the dapp and switch to the same chain you just added
permissions for
7. You should not see any confirmation but the network should change

## **Screenshots/Recordings**

### **Before**


https://github.com/MetaMask/metamask-extension/assets/34557516/6f6321ef-5fb5-48db-84a2-790cf178ea7f

### **After**


https://github.com/MetaMask/metamask-extension/assets/34557516/c94c67be-f17d-4b9d-aec0-431ea89500f5

## **Pre-merge author checklist**

- [ ] I’ve followed [MetaMask Coding
Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
- [ ] I've completed the PR template to the best of my ability

app/scripts/lib/rpc-method-middleware/handlers/ethereum-chain-utils.js
@@ -1,0 +1,241 @@
+import { errorCodes, ethErrors } from 'eth-rpc-errors';
+import { ApprovalType } from '@metamask/controller-utils';
+
+import {
+  BUILT_IN_INFURA_NETWORKS,
+  CHAIN_ID_TO_RPC_URL_MAP,
+  CHAIN_ID_TO_TYPE_MAP,
+  CURRENCY_SYMBOLS,
+  NETWORK_TO_NAME_MAP,
+} from '../../../../../shared/constants/network';
+import {
+  isPrefixedFormattedHexString,
+  isSafeChainId,
+} from '../../../../../shared/modules/network.utils';
+import { CaveatTypes } from '../../../../../shared/constants/permissions';
+import { UNKNOWN_TICKER_SYMBOL } from '../../../../../shared/constants/app';
+import { PermissionNames } from '../../../controllers/permissions';
+import { getValidUrl } from '../../util';
+
+export function findExistingNetwork(chainId, findNetworkConfigurationBy) {
+  if (
+    Object.values(BUILT_IN_INFURA_NETWORKS)
+      .map(({ chainId: id }) => id)
+      .includes(chainId)
+  ) {
+    return {
+      chainId,
+      ticker: CURRENCY_SYMBOLS.ETH,
+      nickname: NETWORK_TO_NAME_MAP[chainId],
+      rpcUrl: CHAIN_ID_TO_RPC_URL_MAP[chainId],
+      type: CHAIN_ID_TO_TYPE_MAP[chainId],
+    };
+  }
+  return findNetworkConfigurationBy({ chainId });
+}
+
+export function validateChainId(chainId) {
+  const _chainId = typeof chainId === 'string' && chainId.toLowerCase();
+  if (!isPrefixedFormattedHexString(_chainId)) {
+    throw ethErrors.rpc.invalidParams({
+      message: `Expected 0x-prefixed, unpadded, non-zero hexadecimal string 'chainId'. Received:\n${chainId}`,
+    });
+  }
+
+  if (!isSafeChainId(parseInt(_chainId, 16))) {
+    throw ethErrors.rpc.invalidParams({
+      message: `Invalid chain ID "${_chainId}": numerical value greater than max safe value. Received:\n${chainId}`,
+    });
+  }
+
+  return _chainId;
+}
+
+export function validateSwitchEthereumChainParams(req, end) {
+  if (!req.params?.[0] || typeof req.params[0] !== 'object') {
+    throw ethErrors.rpc.invalidParams({
+      message: `Expected single, object parameter. Received:\n${JSON.stringify(
+        req.params,
+      )}`,
+    });
+  }
+  const { chainId, ...otherParams } = req.params[0];
+
+  if (Object.keys(otherParams).length > 0) {
+    throw ethErrors.rpc.invalidParams({
+      message: `Received unexpected keys on object parameter. Unsupported keys:\n${Object.keys(
+        otherParams,
+      )}`,
+    });
+  }
+
+  return validateChainId(chainId, end);
+}
+
+export function validateAddEthereumChainParams(params, end) {
+  if (!params || typeof params !== 'object') {
+    throw ethErrors.rpc.invalidParams({
+      message: `Expected single, object parameter. Received:\n${JSON.stringify(
+        params,
+      )}`,
+    });
+  }
+
+  const {
+    chainId,
+    chainName,
+    blockExplorerUrls,
+    nativeCurrency,
+    rpcUrls,
+    ...otherParams
+  } = params;
+
+  if (Object.keys(otherParams).length > 0) {
+    throw ethErrors.rpc.invalidParams({
+      message: `Received unexpected keys on object parameter. Unsupported keys:\n${Object.keys(
+        otherParams,
+      )}`,
+    });
+  }
+
+  const _chainId = validateChainId(chainId, end);
+  if (!rpcUrls || !Array.isArray(rpcUrls) || rpcUrls.length === 0) {
+    throw ethErrors.rpc.invalidParams({
+      message: `Expected an array with at least one valid string HTTPS url 'rpcUrls', Received:\n${rpcUrls}`,
+    });
+  }
+
+  const isLocalhostOrHttps = (urlString) => {
+    const url = getValidUrl(urlString);
+    return (
+      url !== null &&
+      (url.hostname === 'localhost' ||
+        url.hostname === '127.0.0.1' ||
+        url.protocol === 'https:')
+    );
+  };
+
+  const firstValidRPCUrl = rpcUrls.find((rpcUrl) => isLocalhostOrHttps(rpcUrl));
+  const firstValidBlockExplorerUrl =
+    blockExplorerUrls !== null && Array.isArray(blockExplorerUrls)
+      ? blockExplorerUrls.find((blockExplorerUrl) =>
+          isLocalhostOrHttps(blockExplorerUrl),
+        )
+      : null;
+
+  if (!firstValidRPCUrl) {
+    throw ethErrors.rpc.invalidParams({
+      message: `Expected an array with at least one valid string HTTPS url 'rpcUrls', Received:\n${rpcUrls}`,
+    });
+  }
+
+  if (blockExplorerUrls !== null && !firstValidBlockExplorerUrl) {
+    throw ethErrors.rpc.invalidParams({
+      message: `Expected null or array with at least one valid string HTTPS URL 'blockExplorerUrl'. Received: ${blockExplorerUrls}`,
+    });
+  }
+
+  if (typeof chainName !== 'string' || !chainName) {
+    throw ethErrors.rpc.invalidParams({
+      message: `Expected non-empty string 'chainName'. Received:\n${chainName}`,
+    });
+  }
+
+  const _chainName =
+    chainName.length > 100 ? chainName.substring(0, 100) : chainName;
+
+  if (nativeCurrency !== null) {
+    if (typeof nativeCurrency !== 'object' || Array.isArray(nativeCurrency)) {
+      throw ethErrors.rpc.invalidParams({
+        message: `Expected null or object 'nativeCurrency'. Received:\n${nativeCurrency}`,
+      });
+    }
+    if (nativeCurrency.decimals !== 18) {
+      throw ethErrors.rpc.invalidParams({
+        message: `Expected the number 18 for 'nativeCurrency.decimals' when 'nativeCurrency' is provided. Received: ${nativeCurrency.decimals}`,
+      });
+    }
+
+    if (!nativeCurrency.symbol || typeof nativeCurrency.symbol !== 'string') {
+      throw ethErrors.rpc.invalidParams({
+        message: `Expected a string 'nativeCurrency.symbol'. Received: ${nativeCurrency.symbol}`,
+      });
+    }
+  }
+
+  const ticker = nativeCurrency?.symbol || UNKNOWN_TICKER_SYMBOL;
+  if (
+    ticker !== UNKNOWN_TICKER_SYMBOL &&
+    (typeof ticker !== 'string' || ticker.length < 2 || ticker.length > 6)
+  ) {
+    throw ethErrors.rpc.invalidParams({
+      message: `Expected 2-6 character string 'nativeCurrency.symbol'. Received:\n${ticker}`,
+    });
+  }
+
+  return {
+    chainId: _chainId,
+    chainName: _chainName,
+    firstValidBlockExplorerUrl,
+    firstValidRPCUrl,
+    ticker,
+  };
+}
+
+export async function switchChain(
+  res,
+  end,
+  origin,
+  chainId,
+  requestData,
+  networkClientId,
+  approvalFlowId,
+  {
+    getChainPermissionsFeatureFlag,
+    setActiveNetwork,
+    endApprovalFlow,
+    requestUserApproval,
+    getCaveat,
+    requestPermittedChainsPermission,
+  },
+) {
+  try {
+    if (getChainPermissionsFeatureFlag()) {
+      const { value: permissionedChainIds } =
+        getCaveat({
+          target: PermissionNames.permittedChains,
+          caveatType: CaveatTypes.restrictNetworkSwitching,
+        }) ?? {};
+
+      if (
+        permissionedChainIds === undefined ||
+        !permissionedChainIds.includes(chainId)
+      ) {
+        await requestPermittedChainsPermission([
+          ...(permissionedChainIds ?? []),
+          chainId,
+        ]);
+      }
+    } else {
+      await requestUserApproval({
+        origin,
+        type: ApprovalType.SwitchEthereumChain,
+        requestData,
+      });
+    }
+
+    await setActiveNetwork(networkClientId);
+    res.result = null;
+  } catch (error) {
+    return end(
+      error.code === errorCodes.provider.userRejectedRequest
+        ? undefined
+        : error,
+    );
+  } finally {
+    if (approvalFlowId) {
+      endApprovalFlow({ id: approvalFlowId });
+    }
+  }
+  return end();
+}

app/scripts/controllers/permissions/caveat-mutators.js
@@ -10,6 +10,9 @@ export const CaveatMutatorFactories = {
   [CaveatTypes.restrictReturnedAccounts]: {
     removeAccount,
   },
+  [CaveatTypes.restrictNetworkSwitching]: {
+    removeChainId,
+  },
 };
 
 /**
@@ -35,6 +38,33 @@ function removeAccount(targetAccount, existingAccounts) {
     return {
       operation: CaveatMutatorOperation.updateValue,
       value: newAccounts,
+    };
+  }
+  return { operation: CaveatMutatorOperation.revokePermission };
+}
+
+/**
+ * Removes the target chain ID from the value arrays of all
+ * `restrictNetworkSwitching` caveats. No-ops if the target chain ID is not in
+ * the array, and revokes the parent permission if it's the only chain ID in
+ * the array.
+ *
+ * @param {string} targetChainId - The chain ID to remove from
+ * all network switching permissions.
+ * @param {string[]} existingChainIds - The chain ID array from the
+ * network switching permissions.
+ */
+function removeChainId(targetChainId, existingChainIds) {
+  const newChainIds = existingChainIds.filter(
+    (chainId) => chainId !== targetChainId,
+  );
+
+  if (newChainIds.length === existingChainIds.length) {
+    return { operation: CaveatMutatorOperation.noop };
+  } else if (newChainIds.length > 0) {
+    return {
+      operation: CaveatMutatorOperation.updateValue,
+      value: newChainIds,
     };
   }
   return { operation: CaveatMutatorOperation.revokePermission };

app/scripts/controllers/permissions/specifications.js
@@ -1,6 +1,7 @@
 import {
   constructPermission,
   PermissionType,
+  SubjectType,
 } from '@metamask/permission-controller';
 ///: BEGIN:ONLY_INCLUDE_IF(snaps)
 import {
@@ -23,17 +24,22 @@ import {
  * The "keys" of all of permissions recognized by the PermissionController.
  * Permission keys and names have distinct meanings in the permission system.
  */
-const PermissionNames = Object.freeze({
+export const PermissionNames = Object.freeze({
   ...RestrictedMethods,
+  permittedChains: 'permittedChains',
 });
 
 /**
  * Factory functions for all caveat types recognized by the
  * PermissionController.
  */
-const CaveatFactories = Object.freeze({
+export const CaveatFactories = Object.freeze({
   [CaveatTypes.restrictReturnedAccounts]: (accounts) => {
     return { type: CaveatTypes.restrictReturnedAccounts, value: accounts };
+  },
+
+  [CaveatTypes.restrictNetworkSwitching]: (chainIds) => {
+    return { type: CaveatTypes.restrictNetworkSwitching, value: chainIds };
   },
 });
 
@@ -45,7 +51,10 @@ const CaveatFactories = Object.freeze({
  *   getInternalAccounts: () => Record<string, import('@metamask/keyring-api').InternalAccount>,
  * }} options - Options bag.
  */
-export const getCaveatSpecifications = ({ getInternalAccounts }) => {
+export const getCaveatSpecifications = ({
+  getInternalAccounts,
+  findNetworkClientIdByChainId,
+}) => {
   return {
     [CaveatTypes.restrictReturnedAccounts]: {
       type: CaveatTypes.restrictReturnedAccounts,
@@ -59,6 +68,11 @@ export const getCaveatSpecifications = ({ getInternalAccounts }) => {
 
       validator: (caveat, _origin, _target) =>
         validateCaveatAccounts(caveat.value, getInternalAccounts),
+    },
+    [CaveatTypes.restrictNetworkSwitching]: {
+      type: CaveatTypes.restrictNetworkSwitching,
+      validator: (caveat, _origin, _target) =>
+        validateCaveatNetworks(caveat.value, findNetworkClientIdByChainId),
     },
 
     ///: BEGIN:ONLY_INCLUDE_IF(snaps)
@@ -113,7 +127,6 @@ export const getPermissionSpecifications = ({
           ],
         });
       },
-
       methodImplementation: async (_args) => {
         const accounts = await getAllAccounts();
         const internalAccounts = getInternalAccounts();
@@ -162,7 +175,6 @@ export const getPermissionSpecifications = ({
           );
         });
       },
-
       validator: (permission, _origin, _target) => {
         const { caveats } = permission;
         if (
@@ -172,6 +184,43 @@ export const getPermissionSpecifications = ({
         ) {
           throw new Error(
             `${PermissionNames.eth_accounts} error: Invalid caveats. There must be a single caveat of type "${CaveatTypes.restrictReturnedAccounts}".`,
+          );
+        }
+      },
+    },
+
+    [PermissionNames.permittedChains]: {
+      permissionType: PermissionType.Endowment,
+      targetName: PermissionNames.permittedChains,
+      allowedCaveats: [CaveatTypes.restrictNetworkSwitching],
+      subjectTypes: [SubjectType.Website],
+
+      factory: (permissionOptions, requestData) => {
+        if (!requestData.approvedChainIds) {
+          throw new Error(
+            `${PermissionNames.permittedChains}: No approved networks specified.`,
+          );
+        }
+
+        return constructPermission({
+          ...permissionOptions,
+          caveats: [
+            CaveatFactories[CaveatTypes.restrictNetworkSwitching](
+              requestData.approvedChainIds,
+            ),
+          ],
+        });
+      },
+      endowmentGetter: async (_getterOptions) => undefined,
+      validator: (permission, _origin, _target) => {
+        const { caveats } = permission;
+        if (
+          !caveats ||
+          caveats.length !== 1 ||
+          caveats[0].type !== CaveatTypes.restrictNetworkSwitching
+        ) {
+          throw new Error(
+            `${PermissionNames.permittedChains} error: Invalid caveats. There must be a single caveat of type "${CaveatTypes.restrictNetworkSwitching}".`,
           );
         }
       },
@@ -211,6 +260,36 @@ function validateCaveatAccounts(accounts, getInternalAccounts) {
     ) {
       throw new Error(
         `${PermissionNames.eth_accounts} error: Received unrecognized address: "${address}".`,
+      );
+    }
+  });
+}
+
+/**
+ * Validates the networks associated with a caveat. Ensures that
+ * the networks value is an array of valid chain IDs.
+ *
+ * @param {string[]} chainIdsForCaveat - The list of chain IDs to validate.
+ * @param {function(string): string} findNetworkClientIdByChainId - Function to find network client ID by chain ID.
+ * @throws {Error} If the chainIdsForCaveat is not a non-empty array of valid chain IDs.
+ */
+function validateCaveatNetworks(
+  chainIdsForCaveat,
+  findNetworkClientIdByChainId,
+) {
+  if (!Array.isArray(chainIdsForCaveat) || chainIdsForCaveat.length === 0) {
+    throw new Error(
+      `${PermissionNames.permittedChains} error: Expected non-empty array of chainIds.`,
+    );
+  }
+
+  chainIdsForCaveat.forEach((chainId) => {
+    try {
+      findNetworkClientIdByChainId(chainId);
+    } catch (e) {
+      console.error(e);
+      throw new Error(
+        `${PermissionNames.permittedChains} error: Received unrecognized chainId: "${chainId}". Please try adding the network first via wallet_addEthereumChain.`,
       );
     }
   });
@@ -278,6 +357,7 @@ export const unrestrictedMethods = Object.freeze([
   'net_version',
   'personal_ecRecover',
   'personal_sign',
+  'wallet_switchEthereumChain',
   'wallet_watchAsset',
   'web3_clientVersion',
   'web3_sha3',

app/scripts/controllers/permissions/specifications.test.js
@@ -17,10 +17,13 @@ describe('PermissionController specifications', () => {
   describe('caveat specifications', () => {
     it('getCaveatSpecifications returns the expected specifications object', () => {
       const caveatSpecifications = getCaveatSpecifications({});
-      expect(Object.keys(caveatSpecifications)).toHaveLength(12);
+      expect(Object.keys(caveatSpecifications)).toHaveLength(13);
       expect(
         caveatSpecifications[CaveatTypes.restrictReturnedAccounts].type,
       ).toStrictEqual(CaveatTypes.restrictReturnedAccounts);
+      expect(
+        caveatSpecifications[CaveatTypes.restrictNetworkSwitching].type,
+      ).toStrictEqual(CaveatTypes.restrictNetworkSwitching);
 
       expect(caveatSpecifications.permittedDerivationPaths.type).toStrictEqual(
         SnapCaveatType.PermittedDerivationPaths,
@@ -181,10 +184,13 @@ describe('PermissionController specifications', () => {
   describe('permission specifications', () => {
     it('getPermissionSpecifications returns the expected specifications object', () => {
       const permissionSpecifications = getPermissionSpecifications({});
-      expect(Object.keys(permissionSpecifications)).toHaveLength(1);
+      expect(Object.keys(permissionSpecifications)).toHaveLength(2);
       expect(
         permissionSpecifications[RestrictedMethods.eth_accounts].targetName,
       ).toStrictEqual(RestrictedMethods.eth_accounts);
+      expect(permissionSpecifications.permittedChains.targetName).toStrictEqual(
+        'permittedChains',
+      );
     });
 
     describe('eth_accounts', () => {

aimaniac1989/metamask-review

commit 7f9aa8ab8d4749781eb22356e8640a0bbaa3bdc1

fix(cherry-pick): Fix `wallet_addEthereumChain`/`wallet_switchEthereumChain` error handling bug (#25339)
## **Description**
A bug was introduced in
https://github.com/MetaMask/metamask-extension/pull/24415, where user
rejected request errors are not correctly handled in either
`wallet_switchEthereumChain` nor `wallet_addEthereumChain` calls.

At first we thought his just resulted in incorrect errors being returned
to dapps when users clicked cancel on `wallet_switchEthereumChain` calls
but it turns out this incorrect error handling results in very strange
and severe bugs when subsequent requests are enqueued in the
`QueuedRequestController` - the functionality of which is turned on by
default in release v12.0.0. See the `Before` video below.

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/25339?quickstart=1)

## **Related issues**

See:
https://consensys.slack.com/archives/CTQAGKY5V/p1718385169900809?thread_ts=1718140104.578969&cid=CTQAGKY5V

## **Manual testing steps**
(have gnosis chain added to the wallet)
1. Go to https://metamask.github.io/test-dapp/ and connect 
2. Go to
https://docs.metamask.io/wallet/reference/wallet_switchethereumchain/
3. Submit the switch chain request on the dapp but do not confirm or
reject the confirmation
4. Go back to https://metamask.github.io/test-dapp/
5. Click `Send EIP-1559 Transaction`, see that the badge number in the
MM tray icon increments
6. Open the pop up and cancel the still pending switchChain confirmation
7. You should see the transactions you initiated on
https://metamask.github.io/test-dapp/ pop up now

## **Screenshots/Recordings**

<!-- If applicable, add screenshots and/or recordings to visualize the
before and after of your change. -->

### **Before**


https://github.com/MetaMask/metamask-extension/assets/34557516/a1e355d3-b39f-4670-975c-99beb878e75b

### **After**

app/scripts/lib/rpc-method-middleware/handlers/ethereum-chain-utils.js
@@ -230,11 +230,19 @@ export async function switchChain(
     await setActiveNetwork(networkClientId);
     res.result = null;
   } catch (error) {
-    return end(
-      error.code === errorCodes.provider.userRejectedRequest
-        ? undefined
-        : error,
-    );
+    // We don't want to return an error if user rejects the request
+    // and this is a chained switch request after wallet_addEthereumChain.
+    // approvalFlowId is only defined when this call is of a
+    // wallet_addEthereumChain request so we can use it to determine
+    // if we should return an error
+    if (
+      error.code === errorCodes.provider.userRejectedRequest &&
+      approvalFlowId
+    ) {
+      res.result = null;
+      return end();
+    }
+    return end(error);
   } finally {
     if (approvalFlowId) {
       endApprovalFlow({ id: approvalFlowId });