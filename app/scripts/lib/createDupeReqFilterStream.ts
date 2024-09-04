MetaMask/metamask-extension

commit 27655eb8e3a125212484ee23a659e85f6c328290

fix(deps): Bump `@metamask/eth-json-rpc-middleware` to `^14.0.0`, `@metamask/transaction-controller` to `^35.1.1` (#26143)
## **Description**

Updates `@metamask/eth-json-rpc-middleware` from `^12.1.1` to `^14.0.0`.
- This version bump comes with a large number of regressions, most of
them type errors.
- This is because the package's dependencies are also updated by
multiple major versions, and the changes include improved, stricter
types (especially in `@metamask/utils`).

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/26143?quickstart=1)

## **Related issues**

- Closes https://github.com/MetaMask/metamask-extension/issues/26287
- Blocks:
  - https://github.com/MetaMask/MetaMask-planning/issues/2991
  - https://github.com/MetaMask/MetaMask-planning/issues/2810
  - https://github.com/MetaMask/metamask-extension/issues/25733

## Changelog

### Added

- Add and export `PPOMMiddlewareRequest` type for `JsonRpcRequest` types
that include the `securityAlertResponse` property.
  - `securityAlertResponse` is defined as both optional and nullable.
- Add `PPOMRequest` type for `eth-sendTransaction` requests.

### Changed

- **BREAKING:** Bump `@metamask/eth-json-rpc-middleware` from `^12.1.1`
to `^14.0.0`.
- **BREAKING:** Bump `@metamask/transaction-controller` from `^34.0.0`
to `^35.1.1`.
- **BREAKING:** Redefine `SecurityAlertsAPIRequest` as a
`JsonRpcRequest` type that accepts `unknown[]` as its `params` type.
- Widen the `request` parameters of the functions
`validateWithController` and `validateWithAPI` to include
`SecurityAlertsAPIRequest`.
- Bump `@trezor/connect-web` from `9.2.2` to `9.3.0`.

### Fixed

- **BREAKING:** Narrow `Params` generic parameter of
`createPPOMMiddleware` function from `JsonRpcParams` to `(string | { to:
string })[]`.
- Add `Params` generic parameter to `handleSnapRequest` function, which

app/scripts/lib/createDupeReqFilterStream.ts
@@ -54,7 +54,8 @@ export default function createDupeReqFilterStream() {
     transform(chunk: JsonRpcRequest, _, cb) {
       // JSON-RPC notifications have no ids; our only recourse is to let them through.
       const hasNoId = chunk.id === undefined;
-      const requestNotYetSeen = seenRequestIds.add(chunk.id);
+      const requestNotYetSeen =
+        chunk.id !== null && seenRequestIds.add(chunk.id);
 
       if (hasNoId || requestNotYetSeen) {
         cb(null, chunk);

.yarn/patches/@trezor-connect-web-npm-9.3.0-040ab10d9a.patch
@@ -1,0 +1,33 @@
+diff --git a/lib/impl/core-in-iframe.js b/lib/impl/core-in-iframe.js
+index c47cf3bff860d6b1855341c00b80fc6c40f9d6d5..275eb0f312ff396819fa406c154a3562842db49d 100644
+--- a/lib/impl/core-in-iframe.js
++++ b/lib/impl/core-in-iframe.js
+@@ -116,7 +116,9 @@ class CoreInIframe {
+         this._log.enabled = !!this._settings.debug;
+         window.addEventListener('message', this.boundHandleMessage);
+         window.addEventListener('unload', this.boundDispose);
+-        await iframe.init(this._settings);
++        const modifiedSettings = Object.assign({}, this.settings);
++        modifiedSettings.env = 'webextension';
++        await iframe.init(modifiedSettings);
+         if (this._settings.sharedLogger !== false) {
+             iframe.initIframeLogger();
+         }
+diff --git a/lib/popup/index.js b/lib/popup/index.js
+index 9b13c370a5ac8b4e4fc0315ed40cdf615d0bb0cb..4dbd97fc28df49beb73379451974ec48a8a42ea7 100644
+--- a/lib/popup/index.js
++++ b/lib/popup/index.js
+@@ -229,10 +229,12 @@ class PopupManager extends events_1.default {
+         }
+         else if (message.type === events_2.POPUP.LOADED) {
+             this.handleMessage(message);
++            const modifiedSettings = Object.assign({}, this.settings);
++            modifiedSettings.env = 'webextension';
+             this.channel.postMessage({
+                 type: events_2.POPUP.INIT,
+                 payload: {
+-                    settings: this.settings,
++                    settings: modifiedSettings,
+                     useCore: true,
+                 },
+             });

app/scripts/lib/accounts/BalancesController.ts
@@ -224,9 +224,10 @@ export class BalancesController extends BaseController<
    * @param accountId - The account ID.
    */
   #getAccount(accountId: string): InternalAccount {
-    const account: InternalAccount = this.#listMultichainAccounts().find(
-      (multichainAccount) => multichainAccount.id === accountId,
-    );
+    const account: InternalAccount | undefined =
+      this.#listMultichainAccounts().find(
+        (multichainAccount) => multichainAccount.id === accountId,
+      );
 
     if (!account) {
       throw new Error(`Unknown account: ${accountId}`);
@@ -247,13 +248,15 @@ export class BalancesController extends BaseController<
     const account = this.#getAccount(accountId);
     const partialState: BalancesControllerState = { balances: {} };
 
-    partialState.balances[account.id] = await this.#getBalances(
-      account.id,
-      account.metadata.snap.id,
-      isBtcMainnetAddress(account.address)
-        ? BTC_MAINNET_ASSETS
-        : BTC_TESTNET_ASSETS,
-    );
+    if (account.metadata.snap) {
+      partialState.balances[account.id] = await this.#getBalances(
+        account.id,
+        account.metadata.snap.id,
+        isBtcMainnetAddress(account.address)
+          ? BTC_MAINNET_ASSETS
+          : BTC_TESTNET_ASSETS,
+      );
+    }
 
     this.update((state: Draft<BalancesControllerState>) => ({
       ...state,
@@ -292,7 +295,7 @@ export class BalancesController extends BaseController<
     return (
       !isEvmAccountType(account.type) &&
       // Non-EVM accounts are backed by a Snap for now
-      account.metadata.snap
+      account.metadata.snap !== undefined
     );
   }
 

app/scripts/lib/createDupeReqFilterStream.test.ts
@@ -3,7 +3,7 @@ import OurReadableStream from 'readable-stream';
 import ReadableStream2 from 'readable-stream-2';
 import ReadableStream3 from 'readable-stream-3';
 
-import type { JsonRpcRequest } from '@metamask/utils';
+import type { JsonRpcNotification, JsonRpcRequest } from '@metamask/utils';
 import createDupeReqFilterStream, {
   THREE_MINUTES,
 } from './createDupeReqFilterStream';
@@ -26,7 +26,7 @@ function createTestStream(output: JsonRpcRequest[] = [], S = Transform) {
 }
 
 function runStreamTest(
-  requests: JsonRpcRequest[] = [],
+  requests: (JsonRpcRequest | JsonRpcNotification)[] = [],
   advanceTimersTime = 10,
   S = Transform,
 ) {
@@ -54,12 +54,12 @@ describe('createDupeReqFilterStream', () => {
     const requests = [
       { id: 1, method: 'foo' },
       { id: 2, method: 'bar' },
-    ];
+    ].map((request) => ({ ...request, jsonrpc: '2.0' as const }));
 
     const expectedOutput = [
       { id: 1, method: 'foo' },
       { id: 2, method: 'bar' },
-    ];
+    ].map((output) => ({ ...output, jsonrpc: '2.0' }));
 
     const output = await runStreamTest(requests);
     expect(output).toEqual(expectedOutput);
@@ -69,18 +69,25 @@ describe('createDupeReqFilterStream', () => {
     const requests = [
       { id: 1, method: 'foo' },
       { id: 1, method: 'foo' }, // duplicate
-    ];
+    ].map((request) => ({ ...request, jsonrpc: '2.0' as const }));
 
-    const expectedOutput = [{ id: 1, method: 'foo' }];
+    const expectedOutput = [{ id: 1, method: 'foo' }].map((output) => ({
+      ...output,
+      jsonrpc: '2.0',
+    }));
 
     const output = await runStreamTest(requests);
     expect(output).toEqual(expectedOutput);
   });
 
   it("lets through requests if they don't have an id", async () => {
-    const requests = [{ method: 'notify1' }, { method: 'notify2' }];
+    const requests = [{ method: 'notify1' }, { method: 'notify2' }].map(
+      (request) => ({ ...request, jsonrpc: '2.0' as const }),
+    );
 
-    const expectedOutput = [{ method: 'notify1' }, { method: 'notify2' }];
+    const expectedOutput = [{ method: 'notify1' }, { method: 'notify2' }].map(
+      (output) => ({ ...output, jsonrpc: '2.0' }),
+    );
 
     const output = await runStreamTest(requests);
     expect(output).toEqual(expectedOutput);
@@ -95,7 +102,7 @@ describe('createDupeReqFilterStream', () => {
       { method: 'notify2' },
       { id: 2, method: 'bar' },
       { id: 3, method: 'baz' },
-    ];
+    ].map((request) => ({ ...request, jsonrpc: '2.0' as const }));
 
     const expectedOutput = [
       { id: 1, method: 'foo' },
@@ -103,7 +110,7 @@ describe('createDupeReqFilterStream', () => {
       { id: 2, method: 'bar' },
       { method: 'notify2' },
       { id: 3, method: 'baz' },
-    ];
+    ].map((output) => ({ ...output, jsonrpc: '2.0' }));
 
     const output = await runStreamTest(requests);
     expect(output).toEqual(expectedOutput);

app/scripts/lib/ppom/ppom-middleware.test.ts
@@ -1,8 +1,4 @@
-import {
-  type Hex,
-  JsonRpcRequestStruct,
-  JsonRpcResponseStruct,
-} from '@metamask/utils';
+import { type Hex, JsonRpcResponseStruct } from '@metamask/utils';
 import * as ControllerUtils from '@metamask/controller-utils';
 
 import { CHAIN_IDS } from '../../../../shared/constants/network';
@@ -12,7 +8,7 @@ import {
   BlockaidResultType,
 } from '../../../../shared/constants/security-provider';
 import { flushPromises } from '../../../../test/lib/timer-helpers';
-import { createPPOMMiddleware } from './ppom-middleware';
+import { createPPOMMiddleware, PPOMMiddlewareRequest } from './ppom-middleware';
 import {
   generateSecurityAlertId,
   handlePPOMError,
@@ -30,6 +26,12 @@ const SECURITY_ALERT_RESPONSE_MOCK: SecurityAlertResponse = {
   securityAlertId: SECURITY_ALERT_ID_MOCK,
   result_type: BlockaidResultType.Malicious,
   reason: BlockaidReason.permitFarming,
+};
+
+const REQUEST_MOCK = {
+  params: [],
+  id: '',
+  jsonrpc: '2.0' as const,
 };
 
 const createMiddleware = (
@@ -117,14 +119,14 @@ describe('PPOMMiddleware', () => {
     });
 
     const req = {
-      ...JsonRpcRequestStruct,
+      ...REQUEST_MOCK,
       method: 'eth_sendTransaction',
       securityAlertResponse: undefined,
     };
 
     await middlewareFunction(
       req,
-      { ...JsonRpcResponseStruct },
+      { ...JsonRpcResponseStruct.TYPE },
       () => undefined,
     );
 
@@ -141,20 +143,20 @@ describe('PPOMMiddleware', () => {
   it('adds loading response to confirmation requests while validation is in progress', async () => {
     const middlewareFunction = createMiddleware();
 
-    const req = {
-      ...JsonRpcRequestStruct,
+    const req: PPOMMiddlewareRequest<(string | { to: string })[]> = {
+      ...REQUEST_MOCK,
       method: 'eth_sendTransaction',
       securityAlertResponse: undefined,
     };
 
     await middlewareFunction(
       req,
-      { ...JsonRpcResponseStruct },
+      { ...JsonRpcResponseStruct.TYPE },
       () => undefined,
     );
 
-    expect(req.securityAlertResponse.reason).toBe(BlockaidReason.inProgress);
-    expect(req.securityAlertResponse.result_type).toBe(
+    expect(req.securityAlertResponse?.reason).toBe(BlockaidReason.inProgress);
+    expect(req.securityAlertResponse?.result_type).toBe(
       BlockaidResultType.Loading,
     );
   });
@@ -165,11 +167,12 @@ describe('PPOMMiddleware', () => {
     });
 
     const req = {
-      ...JsonRpcRequestStruct,
+      ...REQUEST_MOCK,
       method: 'eth_sendTransaction',
       securityAlertResponse: undefined,
     };
 
+    // @ts-expect-error Passing in invalid input for testing purposes
     await middlewareFunction(req, undefined, () => undefined);
 
     expect(req.securityAlertResponse).toBeUndefined();
@@ -183,14 +186,14 @@ describe('PPOMMiddleware', () => {
     });
 
     const req = {
-      ...JsonRpcRequestStruct,
+      ...REQUEST_MOCK,
       method: 'eth_sendTransaction',
       securityAlertResponse: undefined,
     };
 
     await middlewareFunction(
       req,
-      { ...JsonRpcResponseStruct },
+      { ...JsonRpcResponseStruct.TYPE },
       () => undefined,
     );
 
@@ -202,14 +205,14 @@ describe('PPOMMiddleware', () => {
     const middlewareFunction = createMiddleware();
 
     const req = {
-      ...JsonRpcRequestStruct,
+      ...REQUEST_MOCK,
       method: 'eth_someRequest',
       securityAlertResponse: undefined,
     };
 
     await middlewareFunction(
       req,
-      { ...JsonRpcResponseStruct },
+      { ...JsonRpcResponseStruct.TYPE },
       () => undefined,
     );
 
@@ -221,7 +224,7 @@ describe('PPOMMiddleware', () => {
     const middlewareFunction = createMiddleware();
 
     const req = {
-      ...JsonRpcRequestStruct,
+      ...REQUEST_MOCK,
       params: [{ to: INTERNAL_ACCOUNT_ADDRESS }],
       method: 'eth_sendTransaction',
       securityAlertResponse: undefined,
@@ -229,7 +232,7 @@ describe('PPOMMiddleware', () => {
 
     await middlewareFunction(
       req,
-      { ...JsonRpcResponseStruct },
+      { ...JsonRpcResponseStruct.TYPE },
       () => undefined,
     );
 
@@ -249,7 +252,7 @@ describe('PPOMMiddleware', () => {
         '0x935e73edb9ff52e23bac7f7e043a1ecd06d05477',
         'Example password',
       ],
-      jsonrpc: '2.0',
+      jsonrpc: '2.0' as const,
       id: 2974202441,
       origin: 'https://metamask.github.io',
       networkClientId: 'mainnet',
@@ -276,6 +279,7 @@ describe('PPOMMiddleware', () => {
       },
     });
 
+    // @ts-expect-error Passing invalid input for testing purposes
     await middlewareFunction(req, undefined, () => undefined);
 
     expect(req.securityAlertResponse).toBeUndefined();
@@ -287,8 +291,8 @@ describe('PPOMMiddleware', () => {
     const nextMock = jest.fn();
 
     await middlewareFunction(
-      { ...JsonRpcRequestStruct, method: 'eth_sendTransaction' },
-      { ...JsonRpcResponseStruct },
+      { ...REQUEST_MOCK, method: 'eth_sendTransaction' },
+      { ...JsonRpcResponseStruct.TYPE },
       nextMock,
     );
 
@@ -304,12 +308,12 @@ describe('PPOMMiddleware', () => {
     const middlewareFunction = createMiddleware({ error });
 
     const req = {
-      ...JsonRpcRequestStruct,
+      ...REQUEST_MOCK,
       method: 'eth_sendTransaction',
       securityAlertResponse: undefined,
     };
 
-    await middlewareFunction(req, { ...JsonRpcResponseStruct }, nextMock);
+    await middlewareFunction(req, { ...JsonRpcResponseStruct.TYPE }, nextMock);
 
     expect(req.securityAlertResponse).toStrictEqual(
       SECURITY_ALERT_RESPONSE_MOCK,

app/scripts/lib/ppom/ppom-middleware.ts
@@ -28,6 +28,12 @@ const CONFIRMATION_METHODS = Object.freeze([
   ...SIGNING_METHODS,
 ]);
 
+export type PPOMMiddlewareRequest<
+  Params extends JsonRpcParams = JsonRpcParams,
+> = Required<JsonRpcRequest<Params>> & {
+  securityAlertResponse?: SecurityAlertResponse | undefined;
+};
+
 /**
  * Middleware function that handles JSON RPC requests.
  * This function will be called for every JSON RPC request.
@@ -46,7 +52,7 @@ const CONFIRMATION_METHODS = Object.freeze([
  * @returns PPOMMiddleware function.
  */
 export function createPPOMMiddleware<
-  Params extends JsonRpcParams,
+  Params extends (string | { to: string })[],
   Result extends Json,
 >(
   ppomController: PPOMController,
@@ -61,7 +67,7 @@ export function createPPOMMiddleware<
   ) => void,
 ) {
   return async (
-    req: JsonRpcRequest<Params>,
+    req: PPOMMiddlewareRequest<Params>,
     _res: JsonRpcResponse<Result>,
     next: () => void,
   ) => {
@@ -80,13 +86,14 @@ export function createPPOMMiddleware<
         return;
       }
 
-      const { isSIWEMessage } = detectSIWE({ data: req?.params?.[0] });
-      if (isSIWEMessage) {
-        return;
-      }
-
-      if (req.method === MESSAGE_TYPE.ETH_SEND_TRANSACTION) {
-        const { to: toAddress } = req?.params?.[0] ?? {};
+      const data = req.params[0];
+      if (typeof data === 'string') {
+        const { isSIWEMessage } = detectSIWE({ data });
+        if (isSIWEMessage) {
+          return;
+        }
+      } else if (req.method === MESSAGE_TYPE.ETH_SEND_TRANSACTION) {
+        const { to: toAddress } = data ?? {};
         const internalAccounts = accountsController.listAccounts();
         const isToInternalAccount = internalAccounts.some(
           ({ address }) => address?.toLowerCase() === toAddress?.toLowerCase(),

app/scripts/lib/ppom/ppom-util.test.ts
@@ -35,6 +35,8 @@ const CHAIN_ID_MOCK = '0x1';
 const REQUEST_MOCK = {
   method: 'eth_signTypedData_v4',
   params: [],
+  id: '',
+  jsonrpc: '2.0' as const,
 };
 
 const SECURITY_ALERT_RESPONSE_MOCK: SecurityAlertResponse = {

app/scripts/lib/ppom/ppom-util.ts
@@ -20,6 +20,7 @@ import { SecurityAlertResponse } from './types';
 import {
   getSecurityAlertsAPISupportedChainIds,
   isSecurityAlertsAPIEnabled,
+  SecurityAlertsAPIRequest,
   validateWithSecurityAlertsAPI,
 } from './security-alerts-api';
 
@@ -32,6 +33,10 @@ const SECURITY_ALERT_RESPONSE_ERROR = {
   reason: BlockaidReason.errored,
 };
 
+type PPOMRequest = Omit<JsonRpcRequest, 'method' | 'params'> & {
+  method: typeof METHOD_SEND_TRANSACTION;
+  params: [TransactionParams];
+};
 export async function validateRequestWithPPOM({
   ppomController,
   request,
@@ -129,12 +134,18 @@ export async function isChainSupported(chainId: Hex): Promise<boolean> {
   return supportedChainIds.includes(chainId);
 }
 
-function normalizePPOMRequest(request: JsonRpcRequest): JsonRpcRequest {
-  if (request.method !== METHOD_SEND_TRANSACTION) {
+function normalizePPOMRequest(
+  request: PPOMRequest | JsonRpcRequest,
+): PPOMRequest | JsonRpcRequest {
+  if (
+    !((req): req is PPOMRequest => req.method === METHOD_SEND_TRANSACTION)(
+      request,
+    )
+  ) {
     return request;
   }
 
-  const transactionParams = (request.params?.[0] || {}) as TransactionParams;
+  const transactionParams = request.params[0];
   const normalizedParams = normalizeTransactionParams(transactionParams);
 
   return {
@@ -195,7 +206,7 @@ async function findConfirmationBySecurityAlertId(
 
 async function validateWithController(
   ppomController: PPOMController,
-  request: JsonRpcRequest,
+  request: SecurityAlertsAPIRequest | JsonRpcRequest,
 ): Promise<SecurityAlertResponse> {
   const response = (await ppomController.usePPOM((ppom: PPOM) =>
     ppom.validateJsonRpc(request),
@@ -210,7 +221,7 @@ async function validateWithController(
 async function validateWithAPI(
   ppomController: PPOMController,
   chainId: string,
-  request: JsonRpcRequest,
+  request: SecurityAlertsAPIRequest | JsonRpcRequest,
 ): Promise<SecurityAlertResponse> {
   try {
     const response = await validateWithSecurityAlertsAPI(chainId, request);

aimaniac1989/metamask-review

commit 3a59cf79084e99df40ed08e07113ba1db39273c9

fix: revert breaking change to requests with `null` ids (#26143) (#26814)
## **Description**

Fixes issue introduced by:
https://github.com/MetaMask/metamask-extension/pull/26143,
https://github.com/MetaMask/metamask-extension/pull/26626

- Revert breaking change to requests with `null` IDs handled by
`createDupeReqFilterStream`.
- Remove outdated yarn patch for `@trezor/connect-web@9.2.2`.
- Assign type for `getInitialSendStateWithExistingTxState` param.

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/26814?quickstart=1)

## **Related issues**

- https://github.com/MetaMask/metamask-extension/pull/26143
- https://github.com/MetaMask/metamask-extension/pull/26626

## **Manual testing steps**

## **Screenshots/Recordings**

## **Pre-merge author checklist**

- [x] I've followed [MetaMask Contributor
Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
Extension Coding
Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
- [x] I've completed the PR template to the best of my ability
- [x] I’ve included tests if applicable
- [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
if applicable
- [x] I’ve applied the right labels on the PR (see [labeling
guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
Not required for external contributors.

## **Pre-merge reviewer checklist**

- [ ] I've manually tested the PR (e.g. pull and build branch, run the
app, test code being changed).
- [ ] I confirm that this PR addresses all acceptance criteria described
in the ticket it closes and includes the necessary testing evidence such
as recordings and or screenshots.

.yarn/patches/@trezor-connect-web-npm-9.2.2-a4de8e45fc.patch
@@ -1,43 +1,0 @@
-diff --git a/lib/index.js b/lib/index.js
-index 82a21b70dc18597fdbb1c5bfdbebb10f615c723d..04edf4b3a8a5a2bebda8bf907225b502ab5c30ad 100644
---- a/lib/index.js
-+++ b/lib/index.js
-@@ -109,7 +109,9 @@ const init = async (settings = {}) => {
-     _log.enabled = !!_settings.debug;
-     window.addEventListener('message', handleMessage);
-     window.addEventListener('unload', dispose);
--    await iframe.init(_settings);
-+    var modifiedSettings = Object.assign({}, _settings);
-+    modifiedSettings.env = 'webextension';
-+    await iframe.init(modifiedSettings);
-     if (_settings.sharedLogger !== false) {
-         iframe.initIframeLogger();
-     }
-@@ -125,7 +127,9 @@ const call = async (params) => {
-         }
-         _popupManager.request();
-         try {
--            await init(_settings);
-+            var modifiedSettings = Object.assign({}, _settings);
-+            modifiedSettings.env = 'webextension';
-+            await init(modifiedSettings);
-         }
-         catch (error) {
-             if (_popupManager) {
-diff --git a/lib/popup/index.js b/lib/popup/index.js
-index 6948bdb73b381bb72fb8b87fe2006d0046b65acb..d8f80f77728a18a851da3f03a5ed956cb46fe8e6 100644
---- a/lib/popup/index.js
-+++ b/lib/popup/index.js
-@@ -272,9 +272,11 @@ class PopupManager extends events_1.default {
-                 this.popupPromise.resolve();
-             }
-             (_b = this.iframeHandshakePromise) === null || _b === void 0 ? void 0 : _b.promise.then(payload => {
-+                var modifiedSettings = Object.assign({}, this.settings);
-+                modifiedSettings.env = 'webextension';
-                 this.channel.postMessage({
-                     type: events_2.POPUP.INIT,
--                    payload: Object.assign(Object.assign({}, payload), { settings: this.settings }),
-+                    payload: Object.assign(Object.assign({}, payload), { settings: modifiedSettings }),
-                 });
-             });
-         }

app/scripts/lib/createDupeReqFilterStream.ts
@@ -11,7 +11,7 @@ export const THREE_MINUTES = MINUTE * 3;
  * @returns The expiry set.
  */
 const makeExpirySet = () => {
-  const map: Map<string | number, number> = new Map();
+  const map: Map<string | number | null, number> = new Map();
 
   setInterval(() => {
     const cutoffTime = Date.now() - THREE_MINUTES;
@@ -32,7 +32,7 @@ const makeExpirySet = () => {
      * @param value - The value to add.
      * @returns `true` if the value was added, and `false` if it already existed.
      */
-    add(value: string | number) {
+    add(value: string | number | null) {
       if (!map.has(value)) {
         map.set(value, Date.now());
         return true;
@@ -54,8 +54,7 @@ export default function createDupeReqFilterStream() {
     transform(chunk: JsonRpcRequest, _, cb) {
       // JSON-RPC notifications have no ids; our only recourse is to let them through.
       const hasNoId = chunk.id === undefined;
-      const requestNotYetSeen =
-        chunk.id !== null && seenRequestIds.add(chunk.id);
+      const requestNotYetSeen = seenRequestIds.add(chunk.id);
 
       if (hasNoId || requestNotYetSeen) {
         cb(null, chunk);

test/jest/mocks.ts
@@ -9,6 +9,7 @@ import { KeyringTypes } from '@metamask/keyring-controller';
 import { v4 as uuidv4 } from 'uuid';
 import { keyringTypeToName } from '@metamask/accounts-controller';
 import {
+  DraftTransaction,
   draftTransactionInitialState,
   initialState,
 } from '../../ui/ducks/send';
@@ -133,9 +134,9 @@ export const INITIAL_SEND_STATE_FOR_EXISTING_DRAFT = {
   },
 };
 
-// TODO: Define the type of draftTxState
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getInitialSendStateWithExistingTxState = (draftTxState: any) => ({
+export const getInitialSendStateWithExistingTxState = (
+  draftTxState: DraftTransaction & { test: string },
+) => ({
   ...INITIAL_SEND_STATE_FOR_EXISTING_DRAFT,
   draftTransactions: {
     'test-uuid': {