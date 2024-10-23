MetaMask/metamask-extension

commit e858167bb1fee33d4d0be1ab2f8e1ce2b11c7945

refactor: remove UI initialisation callbacks (#26969)

app/scripts/ui.js
@@ -31,16 +31,23 @@ import { setupMultiplex } from './lib/stream-utils';
 import { getEnvironmentType, getPlatform } from './lib/util';
 import metaRPCClientFactory from './lib/metaRPCClientFactory';
 
+const PHISHING_WARNING_PAGE_TIMEOUT = 1 * 1000; // 1 Second
+const PHISHING_WARNING_SW_STORAGE_KEY = 'phishing-warning-sw-registered';
+const METHOD_START_UI_SYNC = 'startUISync';
+
 const container = document.getElementById('app-content');
 
-const ONE_SECOND_IN_MILLISECONDS = 1_000;
-
-// Timeout for initializing phishing warning page.
-const PHISHING_WARNING_PAGE_TIMEOUT = ONE_SECOND_IN_MILLISECONDS;
-
-const PHISHING_WARNING_SW_STORAGE_KEY = 'phishing-warning-sw-registered';
-
 let extensionPort;
+let isUIInitialised = false;
+
+/**
+ * An error thrown if the phishing warning page takes too long to load.
+ */
+class PhishingWarningPageTimeoutError extends Error {
+  constructor() {
+    super('Timeout failed');
+  }
+}
 
 start().catch(log.error);
 
@@ -59,7 +66,7 @@ async function start() {
   });
 
   endTrace({
-    name: 'Load Scripts',
+    name: TraceName.LoadScripts,
     timestamp: performance.timeOrigin + startTime,
   });
 
@@ -69,131 +76,50 @@ async function start() {
   // identify window type (popup, notification)
   const windowType = getEnvironmentType();
 
-  let isUIInitialised = false;
-
   // setup stream to background
   extensionPort = browser.runtime.connect({ name: windowType });
+
   let connectionStream = new PortStream(extensionPort);
 
   const activeTab = await queryCurrentActiveTab(windowType);
 
-  let loadPhishingWarningPage;
+  /*
+   * In case of MV3 the issue of blank screen was very frequent, it is caused by UI initialising before background is ready to send state.
+   * Code below ensures that UI is rendered only after "CONNECTION_READY" or "startUISync"
+   * messages are received thus the background is ready, and ensures that streams and
+   * phishing warning page load only after the "startUISync" message is received.
+   * In case the UI is already rendered, only update the streams.
+   */
+  const messageListener = async (message) => {
+    const method = message?.data?.method;
 
-  if (isManifestV3) {
-    /*
-     * In case of MV3 the issue of blank screen was very frequent, it is caused by UI initialising before background is ready to send state.
-     * Code below ensures that UI is rendered only after "CONNECTION_READY" or "startUISync"
-     * messages are received thus the background is ready, and ensures that streams and
-     * phishing warning page load only after the "startUISync" message is received.
-     * In case the UI is already rendered, only update the streams.
-     */
-    const messageListener = async (message) => {
-      if (message?.data?.method === 'startUISync') {
-        if (isUIInitialised) {
-          // Currently when service worker is revived we create new streams
-          // in later version we might try to improve it by reviving same streams.
-          updateUiStreams();
-        } else {
-          endTrace({ name: TraceName.BackgroundConnect });
-          initializeUiWithTab(activeTab);
-        }
-        await loadPhishingWarningPage();
-      }
-    };
-
-    /**
-     * An error thrown if the phishing warning page takes too long to load.
-     */
-    class PhishingWarningPageTimeoutError extends Error {
-      constructor() {
-        super('Timeout failed');
-      }
+    if (method !== METHOD_START_UI_SYNC) {
+      return;
     }
 
-    /**
-     * Load the phishing warning page temporarily to ensure the service
-     * worker has been registered, so that the warning page works offline.
-     */
-    loadPhishingWarningPage = async function () {
-      // Check session storage for whether we've already initialized the phishing warning
-      // service worker in this browser session and do not attempt to re-initialize if so.
-      const phishingSWMemoryFetch = await browser.storage.session.get(
-        PHISHING_WARNING_SW_STORAGE_KEY,
+    endTrace({ name: TraceName.BackgroundConnect });
+
+    if (isManifestV3 && isUIInitialised) {
+      // Currently when service worker is revived we create new streams
+      // in later version we might try to improve it by reviving same streams.
+      updateUiStreams();
+    } else {
+      await initializeUiWithTab(
+        activeTab,
+        connectionStream,
+        windowType,
+        traceContext,
       );
+    }
 
-      if (phishingSWMemoryFetch[PHISHING_WARNING_SW_STORAGE_KEY]) {
-        return;
-      }
+    if (isManifestV3) {
+      await loadPhishingWarningPage();
+    } else {
+      extensionPort.onMessage.removeListener(messageListener);
+    }
+  };
 
-      const currentPlatform = getPlatform();
-      let iframe;
-
-      try {
-        const extensionStartupPhishingPageUrl = new URL(
-          process.env.PHISHING_WARNING_PAGE_URL,
-        );
-        // The `extensionStartup` hash signals to the phishing warning page that it should not bother
-        // setting up streams for user interaction. Otherwise this page load would cause a console
-        // error.
-        extensionStartupPhishingPageUrl.hash = '#extensionStartup';
-
-        iframe = window.document.createElement('iframe');
-        iframe.setAttribute('src', extensionStartupPhishingPageUrl.href);
-        iframe.setAttribute('sandbox', 'allow-scripts allow-same-origin');
-
-        // Create "deferred Promise" to allow passing resolve/reject to event handlers
-        let deferredResolve;
-        let deferredReject;
-        const loadComplete = new Promise((resolve, reject) => {
-          deferredResolve = resolve;
-          deferredReject = reject;
-        });
-
-        // The load event is emitted once loading has completed, even if the loading failed.
-        // If loading failed we can't do anything about it, so we don't need to check.
-        iframe.addEventListener('load', deferredResolve);
-
-        // This step initiates the page loading.
-        window.document.body.appendChild(iframe);
-
-        // This timeout ensures that this iframe gets cleaned up in a reasonable
-        // timeframe, and ensures that the "initialization complete" message
-        // doesn't get delayed too long.
-        setTimeout(
-          () => deferredReject(new PhishingWarningPageTimeoutError()),
-          PHISHING_WARNING_PAGE_TIMEOUT,
-        );
-
-        await loadComplete;
-
-        // store a flag in sessions storage that we've already loaded the service worker
-        // and don't need to try again
-        if (currentPlatform === PLATFORM_FIREFOX) {
-          // Firefox does not yet support the storage.session API introduced in MV3
-          // Tracked here: https://bugzilla.mozilla.org/show_bug.cgi?id=1687778
-          console.error(
-            'Firefox does not support required MV3 APIs: Phishing warning page iframe and service worker will reload each page refresh',
-          );
-        } else {
-          browser.storage.session.set({
-            [PHISHING_WARNING_SW_STORAGE_KEY]: true,
-          });
-        }
-      } catch (error) {
-        if (error instanceof PhishingWarningPageTimeoutError) {
-          console.warn(
-            'Phishing warning page timeout; page not guaranteed to work offline.',
-          );
-        } else {
-          console.error('Failed to initialize phishing warning page', error);
-        }
-      } finally {
-        if (iframe) {
-          iframe.remove();
-        }
-      }
-    };
-
+  if (isManifestV3) {
     // resetExtensionStreamAndListeners takes care to remove listeners from closed streams
     // it also creates new streams and attaches event listeners to them
     const resetExtensionStreamAndListeners = () => {
@@ -208,63 +134,133 @@ async function start() {
       extensionPort.onDisconnect.addListener(resetExtensionStreamAndListeners);
     };
 
-    extensionPort.onMessage.addListener(messageListener);
     extensionPort.onDisconnect.addListener(resetExtensionStreamAndListeners);
-
-    trace({
-      name: TraceName.BackgroundConnect,
-      parentContext: traceContext,
-    });
-  } else {
-    const messageListener = async (message) => {
-      if (message?.data?.method === 'startUISync') {
-        endTrace({ name: TraceName.BackgroundConnect });
-        initializeUiWithTab(activeTab);
-        extensionPort.onMessage.removeListener(messageListener);
-      }
-    };
-
-    extensionPort.onMessage.addListener(messageListener);
-
-    trace({
-      name: TraceName.BackgroundConnect,
-      parentContext: traceContext,
-    });
   }
 
-  function initializeUiWithTab(tab) {
-    initializeUi(tab, connectionStream, traceContext, (err, store) => {
-      if (err) {
-        // if there's an error, store will be = metamaskState
-        displayCriticalError('troubleStarting', err, store);
-        return;
-      }
-      isUIInitialised = true;
+  trace({
+    name: TraceName.BackgroundConnect,
+    parentContext: traceContext,
+  });
 
-      if (process.env.IN_TEST) {
-        window.document?.documentElement?.classList.add('controller-loaded');
-      }
+  extensionPort.onMessage.addListener(messageListener);
+}
 
-      const state = store.getState();
-      const { metamask: { completedOnboarding } = {} } = state;
+/**
+ * Load the phishing warning page temporarily to ensure the service
+ * worker has been registered, so that the warning page works offline.
+ */
+async function loadPhishingWarningPage() {
+  // Check session storage for whether we've already initialized the phishing warning
+  // service worker in this browser session and do not attempt to re-initialize if so.
+  const phishingSWMemoryFetch = await browser.storage.session.get(
+    PHISHING_WARNING_SW_STORAGE_KEY,
+  );
 
-      if (!completedOnboarding && windowType !== ENVIRONMENT_TYPE_FULLSCREEN) {
-        global.platform.openExtensionInBrowser();
-      }
-    });
+  if (phishingSWMemoryFetch[PHISHING_WARNING_SW_STORAGE_KEY]) {
+    return;
   }
 
-  // Function to update new backgroundConnection in the UI
-  function updateUiStreams() {
-    connectToAccountManager(connectionStream, (err, backgroundConnection) => {
-      if (err) {
-        displayCriticalError('troubleStarting', err);
-        return;
-      }
+  const currentPlatform = getPlatform();
+  let iframe;
 
-      updateBackgroundConnection(backgroundConnection);
+  try {
+    const extensionStartupPhishingPageUrl = new URL(
+      process.env.PHISHING_WARNING_PAGE_URL,
+    );
+    // The `extensionStartup` hash signals to the phishing warning page that it should not bother
+    // setting up streams for user interaction. Otherwise this page load would cause a console
+    // error.
+    extensionStartupPhishingPageUrl.hash = '#extensionStartup';
+
+    iframe = window.document.createElement('iframe');
+    iframe.setAttribute('src', extensionStartupPhishingPageUrl.href);
+    iframe.setAttribute('sandbox', 'allow-scripts allow-same-origin');
+
+    // Create "deferred Promise" to allow passing resolve/reject to event handlers
+    let deferredResolve;
+    let deferredReject;
+    const loadComplete = new Promise((resolve, reject) => {
+      deferredResolve = resolve;
+      deferredReject = reject;
     });
+
+    // The load event is emitted once loading has completed, even if the loading failed.
+    // If loading failed we can't do anything about it, so we don't need to check.
+    iframe.addEventListener('load', deferredResolve);
+
+    // This step initiates the page loading.
+    window.document.body.appendChild(iframe);
+
+    // This timeout ensures that this iframe gets cleaned up in a reasonable
+    // timeframe, and ensures that the "initialization complete" message
+    // doesn't get delayed too long.
+    setTimeout(
+      () => deferredReject(new PhishingWarningPageTimeoutError()),
+      PHISHING_WARNING_PAGE_TIMEOUT,
+    );
+
+    await loadComplete;
+
+    // store a flag in sessions storage that we've already loaded the service worker
+    // and don't need to try again
+    if (currentPlatform === PLATFORM_FIREFOX) {
+      // Firefox does not yet support the storage.session API introduced in MV3
+      // Tracked here: https://bugzilla.mozilla.org/show_bug.cgi?id=1687778
+      console.error(
+        'Firefox does not support required MV3 APIs: Phishing warning page iframe and service worker will reload each page refresh',
+      );
+    } else {
+      browser.storage.session.set({
+        [PHISHING_WARNING_SW_STORAGE_KEY]: true,
+      });
+    }
+  } catch (error) {
+    if (error instanceof PhishingWarningPageTimeoutError) {
+      console.warn(
+        'Phishing warning page timeout; page not guaranteed to work offline.',
+      );
+    } else {
+      console.error('Failed to initialize phishing warning page', error);
+    }
+  } finally {
+    if (iframe) {
+      iframe.remove();
+    }
   }
+}
+
+async function initializeUiWithTab(
+  tab,
+  connectionStream,
+  windowType,
+  traceContext,
+) {
+  try {
+    const store = await initializeUi(tab, connectionStream, traceContext);
+
+    endTrace({ name: TraceName.UIStartup });
+
+    isUIInitialised = true;
+
+    if (process.env.IN_TEST) {
+      window.document?.documentElement?.classList.add('controller-loaded');
+    }
+
+    const state = store.getState();
+    const { metamask: { completedOnboarding } = {} } = state;
+
+    if (!completedOnboarding && windowType !== ENVIRONMENT_TYPE_FULLSCREEN) {
+      global.platform.openExtensionInBrowser();
+    }
+  } catch (err) {
+    displayCriticalError('troubleStarting', err);
+  }
+}
+
+// Function to update new backgroundConnection in the UI
+function updateUiStreams(connectionStream) {
+  const backgroundConnection = connectToAccountManager(connectionStream);
+  updateBackgroundConnection(backgroundConnection);
 }
 
 async function queryCurrentActiveTab(windowType) {
@@ -309,22 +305,14 @@ async function queryCurrentActiveTab(windowType) {
   return { id, title, origin, protocol, url };
 }
 
-function initializeUi(activeTab, connectionStream, traceContext, cb) {
-  connectToAccountManager(connectionStream, (err, backgroundConnection) => {
-    if (err) {
-      cb(err, null);
-      return;
-    }
+async function initializeUi(activeTab, connectionStream, traceContext) {
+  const backgroundConnection = connectToAccountManager(connectionStream);
 
-    launchMetaMaskUi(
-      {
-        activeTab,
-        container,
-        backgroundConnection,
-        traceContext,
-      },
-      cb,
-    );
+  return await launchMetaMaskUi({
+    activeTab,
+    container,
+    backgroundConnection,
+    traceContext,
   });
 }
 
@@ -347,13 +335,18 @@ async function displayCriticalError(errorKey, err, metamaskState) {
  * Establishes a connection to the background and a Web3 provider
  *
  * @param {PortDuplexStream} connectionStream - PortStream instance establishing a background connection
- * @param {Function} cb - Called when controller connection is established
  */
-function connectToAccountManager(connectionStream, cb) {
+function connectToAccountManager(connectionStream) {
   const mx = setupMultiplex(connectionStream);
   const controllerConnectionStream = mx.createStream('controller');
-  setupControllerConnection(controllerConnectionStream, cb);
+
+  const backgroundConnection = setupControllerConnection(
+    controllerConnectionStream,
+  );
+
   setupWeb3Connection(mx.createStream('provider'));
+
+  return backgroundConnection;
 }
 
 /**
@@ -375,9 +368,7 @@ function setupWeb3Connection(connectionStream) {
  * Establishes a streamed connection to the background account manager
  *
  * @param {PortDuplexStream} controllerConnectionStream - PortStream instance establishing a background connection
- * @param {Function} cb - Called when the remote account manager connection is established
  */
-function setupControllerConnection(controllerConnectionStream, cb) {
-  const backgroundRPC = metaRPCClientFactory(controllerConnectionStream);
-  cb(null, backgroundRPC);
+function setupControllerConnection(controllerConnectionStream) {
+  return metaRPCClientFactory(controllerConnectionStream);
 }

ui/index.js
@@ -1,3 +1,4 @@
+import { promisify } from 'util';
 import copyToClipboard from 'copy-to-clipboard';
 import log from 'loglevel';
 import { clone } from 'lodash';
@@ -13,7 +14,7 @@ import { ENVIRONMENT_TYPE_POPUP } from '../shared/constants/app';
 import { COPY_OPTIONS } from '../shared/constants/copy';
 import switchDirection from '../shared/lib/switch-direction';
 import { setupLocale } from '../shared/lib/error-utils';
-import { endTrace, trace, TraceName } from '../shared/lib/trace';
+import { trace, TraceName } from '../shared/lib/trace';
 import * as actions from './store/actions';
 import configureStore from './store/store';
 import {
@@ -59,34 +60,19 @@ export const updateBackgroundConnection = (backgroundConnection) => {
   });
 };
 
-export default function launchMetamaskUi(opts, cb) {
+export default async function launchMetamaskUi(opts) {
   const { backgroundConnection, traceContext } = opts;
 
-  trace({
-    name: TraceName.GetState,
-    parentContext: traceContext,
-  });
+  const metamaskState = await trace(
+    { name: TraceName.GetState, parentContext: traceContext },
+    () => promisify(backgroundConnection.getState.bind(backgroundConnection))(),
+  );
 
-  // check if we are unlocked first
-  backgroundConnection.getState(function (err, metamaskState) {
-    endTrace({ name: TraceName.GetState });
+  const store = await startApp(metamaskState, backgroundConnection, opts);
 
-    if (err) {
-      cb(
-        err,
-        {
-          ...metamaskState,
-        },
-        backgroundConnection,
-      );
-      return;
-    }
+  setupStateHooks(store);
 
-    startApp(metamaskState, backgroundConnection, opts).then((store) => {
-      setupStateHooks(store);
-      cb(null, store);
-    });
-  });
+  return store;
 }
 
 /**
@@ -206,48 +192,50 @@ async function startApp(metamaskState, backgroundConnection, opts) {
 
   await trace(
     { name: TraceName.InitialActions, parentContext: traceContext },
-    async () => {
-      // This block autoswitches chains based on the last chain used
-      // for a given dapp, when there are no pending confimrations
-      // This allows the user to be connected on one chain
-      // for one dapp, and automatically change for another
-      const state = store.getState();
-      const networkIdToSwitchTo = getNetworkToAutomaticallySwitchTo(state);
-      if (networkIdToSwitchTo) {
-        await store.dispatch(
-          actions.automaticallySwitchNetwork(
-            networkIdToSwitchTo,
-            getOriginOfCurrentTab(state),
-          ),
-        );
-      } else if (getSwitchedNetworkDetails(state)) {
-        // It's possible that old details could exist if the user
-        // opened the toast but then didn't close it
-        // Clear out any existing switchedNetworkDetails
-        // if the user didn't just change the dapp network
-        await store.dispatch(actions.clearSwitchedNetworkDetails());
-      }
-
-      // Register this window as the current popup
-      // and set in background state
-      if (
-        getUseRequestQueue(state) &&
-        getEnvironmentType() === ENVIRONMENT_TYPE_POPUP
-      ) {
-        const thisPopupId = Date.now();
-        global.metamask.id = thisPopupId;
-        await store.dispatch(actions.setCurrentExtensionPopupId(thisPopupId));
-      }
-    },
+    () => runInitialActions(store),
   );
 
   trace({ name: TraceName.FirstRender, parentContext: traceContext }, () =>
     render(<Root store={store} />, opts.container),
   );
 
-  endTrace({ name: TraceName.UIStartup });
-
   return store;
+}
+
+async function runInitialActions(store) {
+  const state = store.getState();
+
+  // This block autoswitches chains based on the last chain used
+  // for a given dapp, when there are no pending confimrations
+  // This allows the user to be connected on one chain
+  // for one dapp, and automatically change for another
+  const networkIdToSwitchTo = getNetworkToAutomaticallySwitchTo(state);
+
+  if (networkIdToSwitchTo) {
+    await store.dispatch(
+      actions.automaticallySwitchNetwork(
+        networkIdToSwitchTo,
+        getOriginOfCurrentTab(state),
+      ),
+    );
+  } else if (getSwitchedNetworkDetails(state)) {
+    // It's possible that old details could exist if the user
+    // opened the toast but then didn't close it
+    // Clear out any existing switchedNetworkDetails
+    // if the user didn't just change the dapp network
+    await store.dispatch(actions.clearSwitchedNetworkDetails());
+  }
+
+  // Register this window as the current popup
+  // and set in background state
+  if (
+    getUseRequestQueue(state) &&
+    getEnvironmentType() === ENVIRONMENT_TYPE_POPUP
+  ) {
+    const thisPopupId = Date.now();
+    global.metamask.id = thisPopupId;
+    await store.dispatch(actions.setCurrentExtensionPopupId(thisPopupId));
+  }
 }
 
 /**

aimaniac1989/metamask-review

commit 1f884dab98ae79bff2b539b7386a3f08204a104e

fix: Fix stream re-initialization (#28024)
## **Description**

Streams were not correctly re-initialized after a service worker
restart, causing a broken UI for any affected window.

This bug was introduced in
https://github.com/MetaMask/metamask-extension/pull/26969, which reached
production as part of v12.5.0.

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/28024?quickstart=1)

## **Related issues**

Fixes #28018

## **Manual testing steps**

1. Open a fullscreen page or trigger a dapp confirmation
2. Restart the service worker
3. See that the page is broken

## **Screenshots/Recordings**

<!-- If applicable, add screenshots and/or recordings to visualize the
before and after of your change. -->

### **Before**


https://github.com/user-attachments/assets/38e2d2f4-f555-4a14-87f7-73ff2d95c306

### **After**


https://github.com/user-attachments/assets/1b6d9704-1ce2-4363-bc1d-118f1560b64a

## **Pre-merge author checklist**

- [x] I've followed [MetaMask Contributor
Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
Extension Coding
Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
- [x] I've completed the PR template to the best of my ability
- [ ] I’ve included tests if applicable
- No additional tests have been added yet because this is being
considered for a hotfix. This should be covered either by an integration
or an e2e test (or not). The affected code is not easy to cover
effectively with unit tests.
- [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
if applicable
- [x] I’ve applied the right labels on the PR (see [labeling
guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
Not required for external contributors.

## **Pre-merge reviewer checklist**

app/scripts/ui.js
@@ -104,7 +104,7 @@ async function start() {
     if (isManifestV3 && isUIInitialised) {
       // Currently when service worker is revived we create new streams
       // in later version we might try to improve it by reviving same streams.
-      updateUiStreams();
+      updateUiStreams(connectionStream);
     } else {
       await initializeUiWithTab(
         activeTab,