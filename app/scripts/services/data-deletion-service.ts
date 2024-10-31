aimaniac1989/metamask-review

commit 8f28a210b56d92f731d690094363ed1254d11690

feat: Adding a new controller for Metametrics Data Deletion (#24503)

app/scripts/services/data-deletion-service.ts
@@ -1,0 +1,274 @@
+import { hasProperty, isObject } from '@metamask/utils';
+import {
+  circuitBreaker,
+  ConsecutiveBreaker,
+  ExponentialBackoff,
+  handleAll,
+  type IPolicy,
+  retry,
+  wrap,
+  CircuitState,
+} from 'cockatiel';
+import getFetchWithTimeout from '../../../shared/modules/fetch-with-timeout';
+import { DeleteRegulationStatus } from '../../../shared/constants/metametrics';
+
+const DEFAULT_ANALYTICS_DATA_DELETION_SOURCE_ID =
+  process.env.ANALYTICS_DATA_DELETION_SOURCE_ID ?? 'test';
+const DEFAULT_ANALYTICS_DATA_DELETION_ENDPOINT =
+  process.env.ANALYTICS_DATA_DELETION_ENDPOINT ??
+  'https://metametrics.metamask.test';
+
+/**
+ * The number of times we retry a specific failed request to the data deletion API.
+ */
+export const RETRIES = 3;
+
+/**
+ * The maximum conseutive failures allowed before treating the server as inaccessible, and
+ * breaking the circuit.
+ *
+ * Each update attempt will result (1 + retries) calls if the server is down.
+ */
+export const MAX_CONSECUTIVE_FAILURES = (1 + RETRIES) * 3;
+
+/**
+ * When the circuit breaks, we wait for this period of time (in milliseconds) before allowing
+ * a request to go through to the API.
+ */
+const DEFAULT_CIRCUIT_BREAK_DURATION = 30 * 60 * 1000;
+
+/**
+ * The threshold (in milliseconds) for when a successful request is considered "degraded".
+ */
+const DEFAULT_DEGRADED_THRESHOLD = 5_000;
+
+/**
+ * Type guard for Fetch network responses with a `statusCode` property.
+ *
+ * @param response - A suspected Fetch network response.
+ * @returns A type checked Fetch network response.
+ */
+function isValidResponse(
+  response: unknown,
+): response is { statusCode: number } {
+  return (
+    isObject(response) &&
+    hasProperty(response, 'statusCode') &&
+    typeof response.statusCode === 'number'
+  );
+}
+
+/**
+ * Returns `true` if the parameter is a Fetch network response with a status code that indiciates
+ * server failure.
+ *
+ * @param response - The response to check.
+ * @returns `true` if the response indicates a server failure, `false` otherwise.
+ */
+function onServerFailure(response: unknown) {
+  return isValidResponse(response) && response.statusCode >= 500;
+}
+
+/**
+ * Create a Cockatiel retry policy.
+ *
+ * This policy uses a retry and circuit breaker strategy. Callbacks are accepted for circuit breaks
+ * and degraded responses as well.
+ *
+ * @param args - Arguments
+ * @param args.circuitBreakDuration - The amount of time to wait when the circuit breaks
+ * from too many consecutive failures.
+ * @param args.degradedThreshold - The threshold between "normal" and "degrated" service,
+ * in milliseconds.
+ * @param args.maximumConsecutiveFailures - The maximum number of consecutive failures
+ * allowed before breaking the circuit and pausing further updates.
+ * @param args.onBreak - An event handler for when the circuit breaks, useful for capturing
+ * metrics about network failures.
+ * @param args.onDegraded - An event handler for when the circuit remains closed, but requests
+ * are failing or resolving too slowly (i.e. resolving more slowly than the `degradedThreshold`).
+ * @param args.retries - Number of retry attempts.
+ * @returns A Cockatiel retry policy.
+ */
+function createRetryPolicy({
+  circuitBreakDuration,
+  degradedThreshold,
+  maximumConsecutiveFailures,
+  onBreak,
+  onDegraded,
+  retries,
+}: {
+  circuitBreakDuration: number;
+  degradedThreshold: number;
+  maximumConsecutiveFailures: number;
+  onBreak?: () => void;
+  onDegraded?: () => void;
+  retries: number;
+}) {
+  const retryPolicy = retry(handleAll.orWhenResult(onServerFailure), {
+    maxAttempts: retries,
+    backoff: new ExponentialBackoff(),
+  });
+  const circuitBreakerPolicy = circuitBreaker(handleAll, {
+    halfOpenAfter: circuitBreakDuration,
+    breaker: new ConsecutiveBreaker(maximumConsecutiveFailures),
+  });
+  if (onBreak) {
+    circuitBreakerPolicy.onBreak(onBreak);
+  }
+  if (onDegraded) {
+    retryPolicy.onGiveUp(() => {
+      if (circuitBreakerPolicy.state === CircuitState.Closed) {
+        onDegraded();
+      }
+    });
+    retryPolicy.onSuccess(({ duration }) => {
+      if (
+        circuitBreakerPolicy.state === CircuitState.Closed &&
+        duration > degradedThreshold
+      ) {
+        onDegraded();
+      }
+    });
+  }
+  return wrap(retryPolicy, circuitBreakerPolicy);
+}
+
+/**
+ * A serivce for requesting the deletion of analytics data.
+ */
+export class DataDeletionService {
+  #analyticsDataDeletionEndpoint: string;
+
+  #analyticsDataDeletionSourceId: string;
+
+  #fetchStatusPolicy: IPolicy;
+
+  #createDataDeletionTaskPolicy: IPolicy;
+
+  #fetchWithTimeout: ReturnType<typeof getFetchWithTimeout>;
+
+  /**
+   * Construct a data deletion service.
+   *
+   * @param options - Options.
+   * @param options.analyticsDataDeletionEndpoint - The base URL for the data deletion API.
+   * @param options.analyticsDataDeletionSourceId - The Segment source ID to delete data from.
+   * @param options.circuitBreakDuration - The amount of time to wait when the circuit breaks
+   * from too many consecutive failures.
+   * @param options.degradedThreshold - The threshold between "normal" and "degrated" service,
+   * in milliseconds.
+   * @param options.onBreak - An event handler for when the circuit breaks, useful for capturing
+   * metrics about network failures.
+   * @param options.onDegraded - An event handler for when the circuit remains closed, but requests
+   * are failing or resolving too slowly (i.e. resolving more slowly than the `degradedThreshold`).
+   * @param options.timeout - The timeout allowed for network calls before they are aborted.
+   */
+  constructor({
+    analyticsDataDeletionEndpoint = DEFAULT_ANALYTICS_DATA_DELETION_ENDPOINT,
+    analyticsDataDeletionSourceId = DEFAULT_ANALYTICS_DATA_DELETION_SOURCE_ID,
+    circuitBreakDuration = DEFAULT_CIRCUIT_BREAK_DURATION,
+    degradedThreshold = DEFAULT_DEGRADED_THRESHOLD,
+    onBreak,
+    onDegraded,
+    timeout,
+  }: {
+    analyticsDataDeletionEndpoint?: string;
+    analyticsDataDeletionSourceId?: string;
+    circuitBreakDuration?: number;
+    degradedThreshold?: number;
+    onBreak?: () => void;
+    onDegraded?: () => void;
+    timeout?: number;
+  } = {}) {
+    if (!analyticsDataDeletionEndpoint) {
+      throw new Error('Missing ANALYTICS_DATA_DELETION_ENDPOINT');
+    } else if (!analyticsDataDeletionSourceId) {
+      throw new Error('Missing ANALYTICS_DATA_DELETION_SOURCE_ID');
+    }
+    this.#fetchWithTimeout = getFetchWithTimeout(timeout);
+    this.#analyticsDataDeletionEndpoint = analyticsDataDeletionEndpoint;
+    this.#analyticsDataDeletionSourceId = analyticsDataDeletionSourceId;
+    this.#createDataDeletionTaskPolicy = createRetryPolicy({
+      circuitBreakDuration,
+      degradedThreshold,
+      maximumConsecutiveFailures: MAX_CONSECUTIVE_FAILURES,
+      onBreak,
+      onDegraded,
+      retries: RETRIES,
+    });
+    this.#fetchStatusPolicy = createRetryPolicy({
+      circuitBreakDuration,
+      degradedThreshold,
+      maximumConsecutiveFailures: MAX_CONSECUTIVE_FAILURES,
+      onBreak,
+      onDegraded,
+      retries: RETRIES,
+    });
+  }
+
+  /**
+   * Submit a deletion request.
+   *
+   * We use Segment for this request. Segment calls this deletion request a "regulation", and
+   * returns a "regulation ID" to keep track of this request and get status updates for it.
+   * https://docs.segmentapis.com/tag/Deletion-and-Suppression#operation/createSourceRegulation
+   *
+   * @param metaMetricsId - The ID associated with the analytics data that we will be deleting.
+   * @returns The regulation ID for the deletion request.
+   */
+  async createDataDeletionRegulationTask(
+    metaMetricsId: string,
+  ): Promise<string> {
+    const response = await this.#createDataDeletionTaskPolicy.execute(() =>
+      this.#fetchWithTimeout(
+        `${this.#analyticsDataDeletionEndpoint}/regulations/sources/${
+          this.#analyticsDataDeletionSourceId
+        }`,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/vnd.segment.v1+json' },
+          body: JSON.stringify({
+            regulationType: 'DELETE_ONLY',
+            subjectType: 'USER_ID',
+            subjectIds: [metaMetricsId],
+          }),
+        },
+      ),
+    );
+    if (!response.ok) {
+      throw new Error(
+        `Fetch failed with status '${response.status}' for request`,
+      );
+    }
+    return (await response.json()).data.regulateId;
+  }
+
+  /**
+   * Fetch the status of the given deletion request.
+   * https://docs.segmentapis.com/tag/Deletion-and-Suppression#operation/getRegulation
+   *
+   * @param deleteRegulationId - The Segment "regulation ID" for the deletion request to check.
+   * @returns The status of the given deletion request.
+   */
+  async fetchDeletionRegulationStatus(
+    deleteRegulationId: string,
+  ): Promise<DeleteRegulationStatus> {
+    const response = await this.#fetchStatusPolicy.execute(() =>
+      this.#fetchWithTimeout(
+        `${
+          this.#analyticsDataDeletionEndpoint
+        }/regulations/${deleteRegulationId}`,
+        {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/vnd.segment.v1+json' },
+        },
+      ),
+    );
+    if (!response.ok) {
+      throw new Error(
+        `Fetch failed with status '${response.status}' for request`,
+      );
+    }
+    return (await response.json()).data.regulation.overallStatus;
+  }
+}

.metamaskrc.dist
@@ -7,6 +7,8 @@ INFURA_PROJECT_ID=00000000000
 ;PASSWORD=METAMASK PASSWORD
 ;SEGMENT_WRITE_KEY=
 ;BRIDGE_USE_DEV_APIS=
+;ANALYTICS_DATA_DELETION_SOURCE_ID=
+;ANALYTICS_DATA_DELETION_ENDPOINT=
 ;SWAPS_USE_DEV_APIS=
 ;PORTFOLIO_URL=
 ;TRANSACTION_SECURITY_PROVIDER=

app/scripts/constants/sentry-state.ts
@@ -159,6 +159,11 @@ export const SENTRY_BACKGROUND_STATE = {
     traits: false,
     dataCollectionForMarketing: false,
     marketingCampaignCookieId: true,
+    latestNonAnonymousEventTimestamp: true,
+  },
+  MetaMetricsDataDeletionController: {
+    metaMetricsDataDeletionId: true,
+    metaMetricsDataDeletionTimestamp: true,
   },
   NameController: {
     names: false,

app/scripts/controllers/metametrics-data-deletion/metametrics-data-deletion.test.ts
@@ -1,0 +1,181 @@
+import { ControllerMessenger } from '@metamask/base-controller';
+import {
+  MetaMetricsDataDeletionController,
+  type MetaMetricsDataDeletionControllerMessengerActions,
+} from './metametrics-data-deletion';
+
+describe('MetaMetricsDataDeletionController', () => {
+  describe('createMetaMetricsDataDeletionTask', () => {
+    it('creates a data deletion task and stores ID when user is participating in metrics tracking', async () => {
+      const mockMetaMetricsId = 'mockId';
+      const mockTaskId = 'mockTaskId';
+      const { controller, dataDeletionService } = setupController({
+        options: {
+          getMetaMetricsId: jest.fn().mockReturnValue(mockMetaMetricsId),
+          dataDeletionService: {
+            createDataDeletionRegulationTask: jest
+              .fn()
+              .mockResolvedValue(mockTaskId),
+            fetchDeletionRegulationStatus: jest
+              .fn()
+              .mockResolvedValue('UNKNOWN'),
+          },
+        },
+      });
+
+      await controller.createMetaMetricsDataDeletionTask();
+      expect(
+        dataDeletionService.createDataDeletionRegulationTask,
+      ).toHaveBeenCalledWith(mockMetaMetricsId);
+      expect(
+        dataDeletionService.createDataDeletionRegulationTask,
+      ).toHaveBeenCalledTimes(1);
+      expect(
+        dataDeletionService.fetchDeletionRegulationStatus,
+      ).toHaveBeenCalledTimes(1);
+      expect(controller.state).toStrictEqual({
+        metaMetricsDataDeletionId: mockTaskId,
+        metaMetricsDataDeletionTimestamp: expect.any(Number),
+        metaMetricsDataDeletionStatus: 'UNKNOWN',
+      });
+    });
+    it('creates a data deletion task and stores ID when user is not currently participating in metrics tracking', async () => {
+      const mockMetaMetricsId = 'mockId';
+      const mockTaskId = 'mockTaskId';
+      const { controller, dataDeletionService } = setupController({
+        options: {
+          getMetaMetricsId: jest.fn().mockReturnValue(mockMetaMetricsId),
+          dataDeletionService: {
+            createDataDeletionRegulationTask: jest
+              .fn()
+              .mockResolvedValue(mockTaskId),
+            fetchDeletionRegulationStatus: jest
+              .fn()
+              .mockResolvedValue('UNKNOWN'),
+          },
+        },
+      });
+
+      await controller.createMetaMetricsDataDeletionTask();
+
+      expect(
+        dataDeletionService.createDataDeletionRegulationTask,
+      ).toHaveBeenCalledTimes(1);
+      expect(
+        dataDeletionService.fetchDeletionRegulationStatus,
+      ).toHaveBeenCalledTimes(1);
+      expect(
+        dataDeletionService.createDataDeletionRegulationTask,
+      ).toHaveBeenCalledWith(mockMetaMetricsId);
+      expect(controller.state).toStrictEqual({
+        metaMetricsDataDeletionId: mockTaskId,
+        metaMetricsDataDeletionTimestamp: expect.any(Number),
+        metaMetricsDataDeletionStatus: 'UNKNOWN',
+      });
+    });
+
+    it('fails to creates a data deletion task when user has never participating in metrics tracking', async () => {
+      const { controller } = setupController({
+        options: {
+          getMetaMetricsId: jest.fn().mockReturnValue(null),
+        },
+      });
+      await expect(
+        controller.createMetaMetricsDataDeletionTask(),
+      ).rejects.toThrow();
+      expect(controller.state).toStrictEqual({
+        metaMetricsDataDeletionId: null,
+        metaMetricsDataDeletionTimestamp: expect.any(Number),
+      });
+    });
+  });
+  describe('updateDataDeletionTaskStatus', () => {
+    it('fetches and stores status of the delete regulation using delete regulation ID', async () => {
+      const mockMetaMetricsId = 'mockId';
+      const mockTaskId = 'mockTaskId';
+      const { controller, dataDeletionService } = setupController({
+        options: {
+          getMetaMetricsId: jest.fn().mockReturnValue(mockMetaMetricsId),
+          dataDeletionService: {
+            createDataDeletionRegulationTask: jest
+              .fn()
+              .mockResolvedValue(mockTaskId),
+            fetchDeletionRegulationStatus: jest
+              .fn()
+              .mockResolvedValue('UNKNOWN'),
+          },
+        },
+      });
+      await controller.createMetaMetricsDataDeletionTask();
+      await controller.updateDataDeletionTaskStatus();
+      expect(
+        dataDeletionService.fetchDeletionRegulationStatus,
+      ).toHaveBeenCalledTimes(2);
+      expect(
+        dataDeletionService.fetchDeletionRegulationStatus,
+      ).toHaveBeenCalledWith(mockTaskId);
+      expect(controller.state).toStrictEqual({
+        metaMetricsDataDeletionId: mockTaskId,
+        metaMetricsDataDeletionTimestamp: expect.any(Number),
+        metaMetricsDataDeletionStatus: 'UNKNOWN',
+      });
+    });
+  });
+});
+
+/**
+ * Setup a test controller instance.
+ *
+ * @param options - Setup options.
+ * @param options.options - Controller constructor options.
+ * @returns The test controller, a messenger instance, and related mocks.
+ */
+function setupController({
+  options,
+}: {
+  options?: Partial<
+    ConstructorParameters<typeof MetaMetricsDataDeletionController>[0]
+  >;
+} = {}): {
+  controller: MetaMetricsDataDeletionController;
+  dataDeletionService: ConstructorParameters<
+    typeof MetaMetricsDataDeletionController
+  >[0]['dataDeletionService'];
+  messenger: ControllerMessenger<
+    MetaMetricsDataDeletionControllerMessengerActions,
+    never
+  >;
+} {
+  const messenger = new ControllerMessenger<
+    MetaMetricsDataDeletionControllerMessengerActions,
+    never
+  >();
+  const mockCreateDataDeletionRegulationTaskResponse = 'mockRegulateId';
+  const mockFetchDeletionRegulationStatusResponse = 'UNKNOWN';
+  const mockDataDeletionService = {
+    createDataDeletionRegulationTask: jest
+      .fn()
+      .mockResolvedValue(mockCreateDataDeletionRegulationTaskResponse),
+    fetchDeletionRegulationStatus: jest
+      .fn()
+      .mockResolvedValue(mockFetchDeletionRegulationStatusResponse),
+    ...options?.dataDeletionService,
+  };
+  const constructorOptions = {
+    dataDeletionService: mockDataDeletionService,
+    getMetaMetricsId: jest.fn().mockReturnValue('mockMetaMetricsId'),
+    messenger: messenger.getRestricted({
+      name: 'MetaMetricsDataDeletionController',
+      allowedActions: [],
+      allowedEvents: [],
+    }),
+    ...options,
+  };
+  const controller = new MetaMetricsDataDeletionController(constructorOptions);
+
+  return {
+    controller,
+    dataDeletionService: constructorOptions.dataDeletionService,
+    messenger,
+  };
+}

app/scripts/controllers/metametrics-data-deletion/metametrics-data-deletion.ts
@@ -1,0 +1,183 @@
+import {
+  BaseController,
+  RestrictedControllerMessenger,
+} from '@metamask/base-controller';
+import { PublicInterface } from '@metamask/utils';
+import type { DataDeletionService } from '../../services/data-deletion-service';
+import { DeleteRegulationStatus } from '../../../../shared/constants/metametrics';
+
+// Unique name for the controller
+const controllerName = 'MetaMetricsDataDeletionController';
+
+/**
+ * Timestamp at which regulation response is returned.
+ */
+export type DataDeleteTimestamp = number;
+/**
+ * Regulation Id retuned while creating a delete regulation.
+ */
+export type DataDeleteRegulationId = string | null;
+
+/**
+ * MetaMetricsDataDeletionController controller state
+ * metaMetricsDataDeletionId - Regulation Id retuned while creating a delete regulation.
+ * metaMetricsDataDeletionTimestamp - Timestamp at which the most recent regulation is created/requested for.
+ * metaMetricsDataDeletionStatus - Status of the current delete regulation.
+ */
+export type MetaMetricsDataDeletionState = {
+  metaMetricsDataDeletionId: DataDeleteRegulationId;
+  metaMetricsDataDeletionTimestamp: DataDeleteTimestamp;
+  metaMetricsDataDeletionStatus?: DeleteRegulationStatus;
+};
+
+const getDefaultState = (): MetaMetricsDataDeletionState => {
+  return {
+    metaMetricsDataDeletionId: null,
+    metaMetricsDataDeletionTimestamp: 0,
+  };
+};
+
+// Metadata for the controller state
+const metadata = {
+  metaMetricsDataDeletionId: {
+    persist: true,
+    anonymous: true,
+  },
+  metaMetricsDataDeletionTimestamp: {
+    persist: true,
+    anonymous: true,
+  },
+  metaMetricsDataDeletionStatus: {
+    persist: true,
+    anonymous: true,
+  },
+};
+
+// Describes the action creating the delete regulation task
+export type CreateMetaMetricsDataDeletionTaskAction = {
+  type: `${typeof controllerName}:createMetaMetricsDataDeletionTask`;
+  handler: MetaMetricsDataDeletionController['createMetaMetricsDataDeletionTask'];
+};
+
+// Describes the action to check the existing regulation status
+export type UpdateDataDeletionTaskStatusAction = {
+  type: `${typeof controllerName}:updateDataDeletionTaskStatus`;
+  handler: MetaMetricsDataDeletionController['updateDataDeletionTaskStatus'];
+};
+
+// Union of all possible actions for the messenger
+export type MetaMetricsDataDeletionControllerMessengerActions =
+  | CreateMetaMetricsDataDeletionTaskAction
+  | UpdateDataDeletionTaskStatusAction;
+
+// Type for the messenger of MetaMetricsDataDeletionController
+export type MetaMetricsDataDeletionControllerMessenger =
+  RestrictedControllerMessenger<
+    typeof controllerName,
+    MetaMetricsDataDeletionControllerMessengerActions,
+    never,
+    never,
+    never
+  >;
+
+/**
+ * Controller responsible for maintaining
+ * state related to Metametrics data deletion
+ */
+export class MetaMetricsDataDeletionController extends BaseController<
+  typeof controllerName,
+  MetaMetricsDataDeletionState,
+  MetaMetricsDataDeletionControllerMessenger
+> {
+  #dataDeletionService: PublicInterface<DataDeletionService>;
+
+  #getMetaMetricsId: () => string | null;
+
+  /**
+   * Creates a MetaMetricsDataDeletionController instance.
+   *
+   * @param args - The arguments to this function.
+   * @param args.dataDeletionService - The service used for deleting data.
+   * @param args.messenger - Messenger used to communicate with BaseV2 controller.
+   * @param args.state - Initial state to set on this controller.
+   * @param args.getMetaMetricsId - A function that returns the current MetaMetrics ID.
+   */
+  constructor({
+    dataDeletionService,
+    messenger,
+    state,
+    getMetaMetricsId,
+  }: {
+    dataDeletionService: PublicInterface<DataDeletionService>;
+    messenger: MetaMetricsDataDeletionControllerMessenger;
+    state?: Partial<MetaMetricsDataDeletionState>;
+    getMetaMetricsId: () => string | null;
+  }) {
+    // Call the constructor of BaseControllerV2
+    super({
+      messenger,
+      metadata,
+      name: controllerName,
+      state: { ...getDefaultState(), ...state },
+    });
+    this.#getMetaMetricsId = getMetaMetricsId;
+    this.#dataDeletionService = dataDeletionService;
+    this.#registerMessageHandlers();
+  }
+
+  /**
+   * Constructor helper for registering this controller's messaging system
+   * actions.
+   */
+  #registerMessageHandlers(): void {
+    this.messagingSystem.registerActionHandler(
+      `${controllerName}:createMetaMetricsDataDeletionTask`,
+      this.createMetaMetricsDataDeletionTask.bind(this),
+    );
+
+    this.messagingSystem.registerActionHandler(
+      `${controllerName}:updateDataDeletionTaskStatus`,
+      this.updateDataDeletionTaskStatus.bind(this),
+    );
+  }
+
+  /**
+   * Creating the delete regulation using source regulation
+   *
+   */
+  async createMetaMetricsDataDeletionTask(): Promise<void> {
+    const metaMetricsId = this.#getMetaMetricsId();
+    if (!metaMetricsId) {
+      throw new Error('MetaMetrics ID not found');
+    }
+
+    const deleteRegulateId =
+      await this.#dataDeletionService.createDataDeletionRegulationTask(
+        metaMetricsId,
+      );
+    this.update((state) => {
+      state.metaMetricsDataDeletionId = deleteRegulateId ?? null;
+      state.metaMetricsDataDeletionTimestamp = Date.now();
+    });
+    await this.updateDataDeletionTaskStatus();
+  }
+
+  /**
+   * To check the status of the current delete regulation.
+   */
+  async updateDataDeletionTaskStatus(): Promise<void> {
+    const deleteRegulationId = this.state.metaMetricsDataDeletionId;
+    if (!deleteRegulationId) {
+      throw new Error('Delete Regulation id not found');
+    }
+
+    const deletionStatus =
+      await this.#dataDeletionService.fetchDeletionRegulationStatus(
+        deleteRegulationId,
+      );
+
+    this.update((state) => {
+      state.metaMetricsDataDeletionStatus = deletionStatus ?? undefined;
+    });
+  }
+}

app/scripts/controllers/metametrics.js
@@ -92,6 +92,7 @@ const exceptionsToFilter = {
  * @property {boolean} [participateInMetaMetrics] - The user's preference for
  *  participating in the MetaMetrics analytics program. This setting controls
  *  whether or not events are tracked
+ *  @property {boolean} [latestNonAnonymousEventTimestamp] - The timestamp at which last non anonymous event is tracked.
  * @property {{[string]: MetaMetricsEventFragment}} [fragments] - Object keyed
  *  by UUID with stored fragments as values.
  * @property {Array} [eventsBeforeMetricsOptIn] - Array of queued events added before
@@ -156,6 +157,7 @@ export default class MetaMetricsController {
       metaMetricsId: null,
       dataCollectionForMarketing: null,
       marketingCampaignCookieId: null,
+      latestNonAnonymousEventTimestamp: 0,
       eventsBeforeMetricsOptIn: [],
       traits: {},
       previousUserTraits: {},
@@ -1089,7 +1091,11 @@ export default class MetaMetricsController {
   // Saving segmentApiCalls in controller store in MV3 ensures that events are tracked
   // even if service worker terminates before events are submiteed to segment.
   _submitSegmentAPICall(eventType, payload, callback) {
-    const { metaMetricsId, participateInMetaMetrics } = this.state;
+    const {
+      metaMetricsId,
+      participateInMetaMetrics,
+      latestNonAnonymousEventTimestamp,
+    } = this.state;
     if (!participateInMetaMetrics || !metaMetricsId) {
       return;
     }
@@ -1104,6 +1110,11 @@ export default class MetaMetricsController {
     }
     const modifiedPayload = { ...payload, messageId, timestamp };
     this.store.updateState({
+      ...this.store.getState(),
+      latestNonAnonymousEventTimestamp:
+        modifiedPayload.anonymousId === METAMETRICS_ANONYMOUS_ID
+          ? latestNonAnonymousEventTimestamp
+          : timestamp.valueOf(),
       segmentApiCalls: {
         ...this.store.getState().segmentApiCalls,
         [messageId]: {

commit 9cc3db48147dbe20059f07754b97707907795b60

test: Fix data deletion e2e tests (#28221)
## **Description**

The "Delete MetaMetrics Data" e2e tests were recently broken due to a
change in CI configuration. The code-under-test was written to always
use the environment variable present for the data deletion source ID and
endpoint, but the e2e tests wrongly assumed that it would never be
present.

The service has been updated to use the fallback values for e2e test
builds, even if the environment variable is present.

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/28221?quickstart=1)

## **Related issues**

Fixes CI failure currently on all branches (e.g.
https://app.circleci.com/pipelines/github/MetaMask/metamask-extension/108823/workflows/cbc57b89-8647-4219-b413-24df4fdad95c/jobs/4070258
)

This bug was introduced in #24503, but only began causing failures
recently when CI configuration was updated with these new environment
variables.

## **Manual testing steps**

See that the data deletion e2e tests succeed even with both data
deletion environment variables set.

## **Screenshots/Recordings**

N/A

## **Pre-merge author checklist**

- [x] I've followed [MetaMask Contributor
Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask
Extension Coding
Standards](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/CODING_GUIDELINES.md).
- [x] I've completed the PR template to the best of my ability
- [x] I’ve included tests if applicable
- [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
if applicable
- [x] I’ve applied the right labels on the PR (see [labeling
guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
Not required for external contributors.

## **Pre-merge reviewer checklist**

- [ ] I've manually tested the PR (e.g. pull and build branch, run the

app/scripts/services/data-deletion-service.ts
@@ -12,11 +12,17 @@ import {
 import getFetchWithTimeout from '../../../shared/modules/fetch-with-timeout';
 import { DeleteRegulationStatus } from '../../../shared/constants/metametrics';
 
-const DEFAULT_ANALYTICS_DATA_DELETION_SOURCE_ID =
-  process.env.ANALYTICS_DATA_DELETION_SOURCE_ID ?? 'test';
-const DEFAULT_ANALYTICS_DATA_DELETION_ENDPOINT =
-  process.env.ANALYTICS_DATA_DELETION_ENDPOINT ??
-  'https://metametrics.metamask.test';
+const inTest = process.env.IN_TEST;
+const fallbackSourceId = 'test';
+const fallbackDataDeletionEndpoint = 'https://metametrics.metamask.test';
+
+const DEFAULT_ANALYTICS_DATA_DELETION_SOURCE_ID = inTest
+  ? fallbackSourceId
+  : process.env.ANALYTICS_DATA_DELETION_SOURCE_ID ?? fallbackSourceId;
+const DEFAULT_ANALYTICS_DATA_DELETION_ENDPOINT = inTest
+  ? fallbackDataDeletionEndpoint
+  : process.env.ANALYTICS_DATA_DELETION_ENDPOINT ??
+    fallbackDataDeletionEndpoint;
 
 /**
  * The number of times we retry a specific failed request to the data deletion API.