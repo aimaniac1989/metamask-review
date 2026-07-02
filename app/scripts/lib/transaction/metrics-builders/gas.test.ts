MetaMask/metamask-extension

feat: reintroduce saved gas settings

app/scripts/lib/transaction/metrics-builders/gas.test.ts
@@ -24,11 +24,29 @@ describe('gas builder', () => {
       }),
     );
     expect(result.properties.gas_fee_selected).toBe('dapp_proposed');
+    expect(result.properties.gas_fee_presented).toBe('medium');
     expect(result.sensitiveProperties.max_fee_per_gas).toBe(
       hexWEIToDecGWEI('0x3b9aca00'),
     );
     expect(result.sensitiveProperties.max_priority_fee_per_gas).toBe(
       hexWEIToDecGWEI('0x59682f00'),
     );
+  });
+
+  it('normalizes dapp suggested presented gas fee metrics', async () => {
+    const result = await getGasMetricsProperties(
+      createBuilderRequest({
+        transactionMeta: {
+          ...createBuilderRequest().transactionMeta,
+          userFeeLevel: 'custom',
+          defaultGasEstimates: {
+            estimateType: 'dappSuggested',
+          },
+        } as never,
+      }),
+    );
+
+    expect(result.properties.gas_fee_presented).toBe('dapp_proposed');
+    expect(result.properties.gas_fee_selected).toBe('custom');
   });
 });

aimaniac1989/metamask-review

app/scripts/lib/transaction/metrics-builders/gas.test.ts
@@ -13,9 +13,7 @@ describe('gas builder', () => {
       }),
     );
 
-    expect(result.properties).toStrictEqual({
-      gas_fee_selected: 'dapp_proposed',
-    });
+    expect(result.properties.gas_fee_selected).toBe('dapp_proposed');
     expect(result.sensitiveProperties).toStrictEqual({});
   });
 
@@ -30,5 +28,22 @@ describe('gas builder', () => {
     );
 
     expect(result.properties.gas_fee_selected).toBe('high');
+  });
+
+  it('normalizes dapp suggested presented gas fee metrics', async () => {
+    const result = await getGasMetricsProperties(
+      createBuilderRequest({
+        transactionMeta: {
+          ...createBuilderRequest().transactionMeta,
+          userFeeLevel: 'custom',
+          defaultGasEstimates: {
+            estimateType: 'dappSuggested',
+          },
+        } as never,
+      }),
+    );
+
+    expect(result.properties.gas_fee_presented).toBe('dapp_proposed');
+    expect(result.properties.gas_fee_selected).toBe('custom');
   });
 });