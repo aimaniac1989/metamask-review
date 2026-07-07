df0de1841b2a27cdd2392cde8b0106d26f56e266
Frederic HENG <frederic.heng@consensys.net>
2026-07-07 02:20:01 +0200

    fix: make test/e2e/tests/tron/swap.spec.ts passed

--- test/e2e/tests/tron/mocks/common-tron.ts
+++ test/e2e/tests/tron/mocks/common-tron.ts
@@ -1211,7 +1211,7 @@ export async function mockTronGetChainParameters(
   mockServer: Mockttp,
 ): Promise<MockedEndpoint> {
   return mockServer
-    .forPost(tronInfuraUrl('/wallet/getchainparameters'))
+    .forGet(tronInfuraUrl('/wallet/getchainparameters'))
     .always()
     .thenCallback(() => ({
       statusCode: 200,
